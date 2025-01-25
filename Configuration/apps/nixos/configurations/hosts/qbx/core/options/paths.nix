14093122a1e337aa0b2613e6b36722216f38c8d4
Craole <32288735+Craole@users.noreply.github.com>
2025-01-25 07:39:26 +0000

    pathman

--- Configuration/apps/nixos/configurations/hosts/qbx/core/options/paths.nix
+++ Configuration/apps/nixos/configurations/hosts/qbx/core/options/paths.nix
@@ -7,19 +7,21 @@ let
   inherit (lib.options) mkOption;
   inherit (lib.types) attrs;
 
-  top = "dots";
-  mod = "paths";
-  cfg = config.${top}.${mod};
+  dots = "/home/craole/.dots";
+  dotsBin = "${dots}/Bin";
+  top = "paths";
+  mod = "qbx";
+  cfg = config.dots.${top}.${mod};
 in
 {
-  options.${top}.${mod} = mkOption {
+  options.dots.${top}.${mod} = mkOption {
     description = "Paths related to the dots configuration.";
     default = {
-      base = "/home/craole/Configuration";
-      store = ../../.;
+      base = "${dots}/Configuration/apps/nixos/configurations/hosts/qbx";
+      link = ../../.;
       core = rec {
         base = "${cfg.base}/core";
-        store = "${cfg.store}/core";
+        store = "${cfg.link}/core";
         libraries = "${base}/libraries";
         options = "${base}/options";
         modules = "${base}/modules";
@@ -29,7 +31,7 @@ in
       };
       home = {
         base = "${cfg.base}/home";
-        store = "${cfg.store}/home";
+        store = "${cfg.link}/home";
       };
       conf = rec {
         base = "${cfg.base}/.config";
@@ -47,6 +49,8 @@ in
       DOTS_CONF = cfg.conf.base;
       DOTS_BIN = cfg.core.bin;
     };
-    shellInit = with cfg.core; ''[ -f ${pathman} ] && . ${pathman} --action append --dir ${bin}'';
+    shellInit =
+      with cfg.core;
+      ''[ -f ${pathman} ] && . ${pathman} --action append --dir ${bin} --dir ${dotsBin}'';
   };
 }
