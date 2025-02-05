e781d8e1101445a9bf0df0482b3312d2fbafa6ff
Craole <32288735+Craole@users.noreply.github.com>
2025-02-04 23:21:20 -0500

    treefmt is now fmtree

--- shell.nix
+++ shell.nix
@@ -25,22 +25,10 @@ pkgs.mkShell {
     fend
     fastfetch
     treefmt2
+    yamlfmt
   ];
 
   shellHook = ''
-    . /home/craole/.dots/Configuration/bash/config.bash
-    DOTS="$HOME/.dots"
-    DOTS_BIN="$DOTS/Bin"
-    DOTS_CFG="$DOTS/Configuration"
-    export DOTS DOTS_BIN DOTS_CFG
-
-    FASTFETCH_CONFIG="$DOTS_CFG/fastfetch/config.jsonc"
-    STARSHIP_CONFIG="$DOTS_CFG/starship/config.toml"
-    TREEFMT_CONFIG="$DOTS_CFG/treefmt/config.toml"
-
-    . "$DOTS_BIN/utility/files/pathman" --append "$DOTS_BIN"
-    fastfetch --config "$FASTFETCH_CONFIG"
-
     eval "$(direnv hook bash)"
     eval "$(zoxide init bash)"
     eval "$(thefuck --alias)"
