b912b3e1acf53211b747621d1141d8323f1c1cca
Craole <32288735+Craole@users.noreply.github.com>
2025-02-14 18:38:52 -0500

    mpv flake

--- Flakes/media/flake.nix
+++ Flakes/media/flake.nix
@@ -24,56 +24,32 @@
             inherit system;
             config.allowUnfree = true;
           };
-
-          inherit (pkgs) substituteAll mkShell;
-          mpdConfig = substituteAll {
-            src = ./modules/mpd.conf;
-          };
-
-          mpvConfig = substituteAll {
-            src = ./modules/mpv.conf;
-            ytdlp = pkgs.yt-dlp;
-          };
-
-          mpvScript = substituteAll {
-            src = ./modules/mpv.sh;
-            isExecutable = true;
-            mpv = pkgs.mpv;
-          };
-
-          mpvScripts = with pkgs.mpvScripts; [
-            uosc
-            memo
-            quack
-            mpris
-            reload
-            cutter
-            evafast
-            autosub
-            smartskip
-            skipsilence
-            chapterskip
-            sponsorblock
-            quality-menu
-            inhibit-gnome
-            mpv-notify-send
-            webtorrent-mpv-hook
-            mpv-playlistmanager
-          ];
-
-          ytdScript = substituteAll {
-            src = ./modules/ytd.sh;
-            isExecutable = true;
-            ytdlp = pkgs.yt-dlp;
-          };
         in {
-          default = mkShell {
+          default = pkgs.mkShell {
             buildInputs = with pkgs; [
               #| Video
-              freetube
               (mpv.override {
-                scripts = mpvScripts;
+                scripts = with mpvScripts; [
+                  uosc
+                  memo
+                  quack
+                  mpris
+                  reload
+                  cutter
+                  evafast
+                  autosub
+                  smartskip
+                  skipsilence
+                  chapterskip
+                  sponsorblock
+                  quality-menu
+                  inhibit-gnome
+                  mpv-notify-send
+                  webtorrent-mpv-hook
+                  mpv-playlistmanager
+                ];
               })
+              freetube
               mpvc
               yt-dlp
 
@@ -102,41 +78,6 @@
               socat
               xclip
             ];
-
-            shellHook = ''
-              printf "🎬 Comprehensive Media Environment Loaded!"
-
-              #@ Set up directory structure
-              # mkdir -p {bin,config/{mpv/scripts,mpd/playlists},music,videos}
-
-              #@ Copy and process config files
-              cp -f ${mpvScript} ./bin/mpv
-              cp -f ${ytdScript} ./bin/ytd
-              cp -f ${mpvConfig} ./config/mpv/mpv.conf
-              cp -f ${mpdConfig} ./config/mpd/mpd.conf
-
-              #@ Set up executable scripts
-              chmod +x bin/*
-              PATH="$PATH:$PWD/bin"
-              export PATH
-
-              #@ Set up aliases
-              alias radio='curseradio'
-
-              #@ Print usage message
-              printf "\n\nVideo Tools:"
-              printf "\n  mpv      - Enhanced MPV with custom config"
-              printf "\n  ytd      - Download videos (usage: ytd <url> [quality])"
-
-              printf "\n\nImage Viewers:"
-              printf "\n  feh      - Light image viewer"
-              printf "\n  imv      - Alternative image viewer"
-
-              printf "\n\nMusic & Radio:"
-              printf "\n  ncmpcpp  - Music player (music dir: music)"
-              printf "\n  radio    - Terminal radio"
-              echo
-            '';
           };
         };
       })
--- Flakes/media/modules/mpv.sh
+++ Flakes/media/modules/mpv.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 CFG_DIR="${PWD}/config/mpv"
-mkdir -p "$CFG_DIR/scripts"
+# mkdir -p "$CFG_DIR/scripts"
 
 #@ Copy the ytdl_hook script if it doesn't exist
-if [ ! -f "$CFG_DIR/scripts/ytdl_hook.lua" ]; then
-	cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$CFG_DIR/scripts/"
-fi
+# if [ ! -f "$CFG_DIR/scripts/ytdl_hook.lua" ]; then
+# 	cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$CFG_DIR/scripts/"
+# fi
 
 #@ Run MPV with configs
 @mpv@/bin/mpv --config-dir="$CFG_DIR" "$@"
