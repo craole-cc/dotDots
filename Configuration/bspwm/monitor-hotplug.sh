cc8f194cc62d61546b9b575f61d6c36a2557c969
Craole <32288735+Craole@users.noreply.github.com>
2025-02-13 21:06:53 -0500

    symlinked treefmt.toml

--- Configuration/bspwm/monitor-hotplug.sh
+++ Configuration/bspwm/monitor-hotplug.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 for monitor in $(bspc query -M --names); do
 	monitors+=("$monitor")
@@ -7,9 +7,9 @@ done
 
 if [ ${#monitors[@]} == 1 ]; then
 	# Single Monitor Setup
-#	xrandr --output ${monitors[0]} --primary --mode 1920x1080 --pos 0x0
-	bspc monitor ${monitors[0]} -d 1 2 3 4 5 6 7 8 9 10
-elif xrandr | grep ' connected' | grep -q "DP2-2" &&  xrandr | grep ' connected' | grep -q "eDP1"; then
+	#	xrandr --output ${monitors[0]} --primary --mode 1920x1080 --pos 0x0
+	bspc monitor "${monitors[0]}" -d 1 2 3 4 5 6 7 8 9 10
+elif xrandr | grep ' connected' | grep -q "DP2-2" && xrandr | grep ' connected' | grep -q "eDP1"; then
 	# Home Docking Setup
 	xrandr --output DP2-2 --primary --mode 2560x1080 --pos 0x0
 	xrandr --output eDP1 --mode 1920x1080 --pos 2560x0
@@ -17,6 +17,6 @@ elif xrandr | grep ' connected' | grep -q "DP2-2" &&  xrandr | grep ' connected'
 	bspc monitor eDP1 -d 9 10
 else
 	for monitor in $(bspc query -M); do
-		bspc monitor $monitor -d 1 2 3 4 5
+		bspc monitor "$monitor" -d 1 2 3 4 5
 	done
 fi
