14a901f5fa358e68349085bc6fe86e193cca5ea1
Craole <32288735+Craole@users.noreply.github.com>
2025-02-14 15:09:52 -0500

    adding Flakes

---
+++ Flakes/media/modules/ytd.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  printf "Usage: yt-download <url> [quality]\n"
+  printf "Quality options: best, 1080p, 720p, 480p, audio\n"
+  exit 1
+fi
+
+URL="$1"
+QUALITY="${2:-1080p}"
+
+case $QUALITY in
+"best")
+  @yt-dlp@/bin/yt-dlp "$URL"
+  ;;
+"audio")
+  @yt-dlp@/bin/yt-dlp -x --audio-format mp3 "$URL"
+  ;;
+*)
+  @yt-dlp@/bin/yt-dlp -f "bestvideo[height<=${QUALITY%p}]+bestaudio/best[height<=${QUALITY%p}]" "$URL"
+  ;;
+esac
