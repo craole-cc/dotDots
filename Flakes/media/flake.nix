44d3e08cc9c7a2b30814537585a035f26e2f4e7f
Craole <32288735+Craole@users.noreply.github.com>
2025-02-14 17:55:07 -0500

    mpv flake

--- Flakes/media/flake.nix
+++ Flakes/media/flake.nix
@@ -1,22 +1,29 @@
-# flake.nix
 {
   description = "Comprehensive media environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    devShells = nixpkgs.lib.genAttrs supportedSystems (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) substituteAll mkShell;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
 
+        inherit (pkgs) substituteAll mkShell;
         mpdConfig = substituteAll {
           src = ./modules/mpd.conf;
         };
@@ -38,29 +45,21 @@
           ytdlp = pkgs.yt-dlp;
         };
       in {
-        nixpkgs.config.allowUnfree = true;
-        devShells.default = mkShell {
+        default = mkShell {
           buildInputs = with pkgs;
             [
-              #| Video tools
               mpv
               mpvc
               yt-dlp
               ffmpeg
-
-              #| Image viewers
               feh
               imv
-
-              #| Music and radio
               ncmpcpp
               mpc-cli
               mpd
               curseradio
               playerctl
               pamixer
-
-              #| Additional utilities
               btop
               curl
               fzf
@@ -94,24 +93,19 @@
           shellHook = ''
             printf "🎬 Comprehensive Media Environment Loaded!"
 
-            #@ Set up directory structure
             mkdir -p {bin,config/{mpv/scripts,mpd/playlists},music,videos}
 
-            #@ Copy and process config files
             cp -f ${mpvScript} ./bin/mpv
             cp -f ${ytdScript} ./bin/ytd
             cp -f ${mpvConfig} ./config/mpv/mpv.conf
             cp -f ${mpdConfig} ./config/mpd/mpd.conf
 
-            #@ Set up executable scripts
             chmod +x bin/*
             PATH="$PATH:$PWD/bin"
             export PATH
 
-            #@ Set up aliases
             alias radio='curseradio'
 
-            #@ Print usage message
             printf "\n\nVideo Tools:"
             printf "\n  mpv      - Enhanced MPV with custom config"
             printf "\n  ytd      - Download videos (usage: ytd <url> [quality])"
@@ -128,4 +122,5 @@
         };
       }
     );
+  };
 }
