5780341653724f4c7c0a4f70c2ca06a618881d2a
Craole <32288735+Craole@users.noreply.github.com>
2025-02-14 15:16:02 -0500

    mpv flake

--- Flakes/media/modules/mpv.sh
+++ Flakes/media/modules/mpv.sh
@@ -5,7 +5,7 @@ mkdir -p "$CFG_DIR/scripts"
 
 #@ Copy the ytdl_hook script if it doesn't exist
 if [ ! -f "$CFG_DIR/scripts/ytdl_hook.lua" ]; then
-  cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$CFG_DIR/scripts/"
+	cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$CFG_DIR/scripts/"
 fi
 
 #@ Run MPV with configs
--- Flakes/media/modules/ytd.sh
+++ Flakes/media/modules/ytd.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-  printf "Usage: yt-download <url> [quality]\n"
-  printf "Quality options: best, 1080p, 720p, 480p, audio\n"
-  exit 1
+	printf "Usage: yt-download <url> [quality]\n"
+	printf "Quality options: best, 1080p, 720p, 480p, audio\n"
+	exit 1
 fi
 
 URL="$1"
 QUALITY="${2:-1080p}"
 
 case $QUALITY in
-"best")
-  @ytdlp@/bin/yt-dlp "$URL"
-  ;;
-"audio")
-  @ytdlp@/bin/yt-dlp -x --audio-format mp3 "$URL"
-  ;;
-*)
-  @ytdlp@/bin/yt-dlp -f "bestvideo[height<=${QUALITY%p}]+bestaudio/best[height<=${QUALITY%p}]" "$URL"
-  ;;
+	"best")
+		@ytdlp@/bin/yt-dlp "$URL"
+		;;
+	"audio")
+		@ytdlp@/bin/yt-dlp -x --audio-format mp3 "$URL"
+		;;
+	*)
+		@ytdlp@/bin/yt-dlp -f "bestvideo[height<=${QUALITY%p}]+bestaudio/best[height<=${QUALITY%p}]" "$URL"
+		;;
 esac
