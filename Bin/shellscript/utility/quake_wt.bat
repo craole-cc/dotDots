48855069785d5239ac74ae8935883226efe6aa0f
Craole <32288735+Craole@users.noreply.github.com>
2025-02-15 01:33:53 -0500

    set treefmt as the default formatter in vscode

--- Bin/shellscript/utility/quake_wt.bat
+++ Bin/shellscript/utility/quake_wt.bat
@@ -1,1 +1,2 @@
-wt --window _quake "C:\Program Files\PowerShell\7-preview\pwsh.exe" -NoLogo -WindowStyle Minimized; new-tab "C:\\Program Files\\Git\\bin\\bash.exe"
+wt --window _quake "C:\Program Files\PowerShell\7-preview\pwsh.exe" -NoLogo -WindowStyle Minimized
+new-tab "C:\\Program Files\\Git\\bin\\bash.exe"
