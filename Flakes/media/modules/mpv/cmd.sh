12a1ecf69191953ed71af4b5af480b8e37edbe94
Craole <32288735+Craole@users.noreply.github.com>
2025-02-15 01:30:59 -0500

    set treefmt as the default formatter in vscode

--- Flakes/media/modules/mpv/cmd.sh
+++ Flakes/media/modules/mpv/cmd.sh
@@ -5,7 +5,7 @@ mkdir -p "$MPV_CFG_DIR/scripts"
 
 #@ Copy the ytdl_hook script if it doesn't exist
 if [ ! -f "$MPV_CFG_DIR/scripts/ytdl_hook.lua" ]; then
-    cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$MPV_CFG_DIR/scripts/"
+  cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$MPV_CFG_DIR/scripts/"
 fi
 
 #@ Run the command with options
