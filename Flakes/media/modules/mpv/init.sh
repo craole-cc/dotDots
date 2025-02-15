d6699aab2667b008369022d8c69cb1296b44dc5b
Craole <32288735+Craole@users.noreply.github.com>
2025-02-14 21:35:01 -0500

    mpv flake

---
+++ Flakes/media/modules/mpv/init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+MPV_CFG_DIR="${PWD}/config/mpv"
+mkdir -p "$MPV_CFG_DIR/scripts"
+
+#@ Copy the ytdl_hook script if it doesn't exist
+if [ ! -f "$MPV_CFG_DIR/scripts/ytdl_hook.lua" ]; then
+  cp "@mpv@/share/mpv/scripts/ytdl_hook.lua" "$MPV_CFG_DIR/scripts/"
+fi
+
+#@ Define the mpv command
+mpv() {
+  @mpv@/bin/mpv --config-dir="$MPV_CFG_DIR" "$@"
+}
