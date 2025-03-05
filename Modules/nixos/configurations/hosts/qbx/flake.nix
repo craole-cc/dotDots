515e859451402d56e92d57f2b038b6d3faa468c0
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-04 22:08:11 -0500

    added flake for easy repl

--- Modules/nixos/configurations/hosts/qbx/flake.nix
+++ Modules/nixos/configurations/hosts/qbx/flake.nix
@@ -7,10 +7,27 @@
 
   outputs =
     { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+    in
     {
       qbx = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./. ];
       };
+
+      devShells.${system}.default = nixpkgs.mkShell {
+        buildInputs = with nixpkgs; [
+          treefmt2
+          nixfmt-rfc-style
+          nixd
+          shfmt
+          shellcheck
+          bashInteractive
+        ];
+      };
+
+      referencedDevShell = self.devShells.x86_64-linux.default;
+      sourceInfo = self.sourceInfo;
     };
 }
