08deb74fa155399d1772c410e9a3f5909811c548
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-04 21:25:20 -0500

    added flake for easy repl

---
+++ Modules/nixos/configurations/hosts/qbx/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    {
+      nixosConfigurations =
+        flake-utils.lib.eachSystem
+          [
+            "aarch64-linux"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            nixpkgs.lib.nixosSystem {
+              inherit system;
+              modules = [
+                ./.
+              ];
+            }
+          );
+    };
+}
