41b1fd98506abb353283be788eec4a1973ea01dc
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-04 21:06:18 -0500

    Working on qbx

---
+++ Modules/nixos/configurations/hosts/qbx/default.nix
@@ -0,0 +1,40 @@
+{
+  imports = [
+    ./core
+    ./custom
+    ./desktop
+    ./libraries
+    # ./programs
+    # ./services
+    ./users
+  ];
+
+  dots = {
+    desktop.login = {
+      manager = "sddm";
+      automatically = true;
+      user = "craole";
+    };
+
+    users = {
+      craole = {
+        enable = true;
+        # display = "sddm";
+        # desktop = "gnome";
+        # manager = "hyprland";
+        # protocol = "wayland";
+      };
+      qyatt= {
+        # enable = true;
+        # display = "sddm";
+        # desktop = "gnome";
+        # manager = "hyprland";
+        # protocol = "wayland";
+      };
+      cc = {
+        enable = true;
+        isSystemUser = true;
+      };
+    };
+  };
+}
