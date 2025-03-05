ad90e8da3820e4eb5c020eec69998a66099e89df
Craole <32288735+Craole@users.noreply.github.com>
2025-03-05 16:16:37 -0500

    can we do it without flake utils

--- flake.nix
+++ flake.nix
@@ -3,28 +3,28 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixosUnstable.url = "nixpkgs/nixos-unstable";
-    nixosStable.url = "nixpkgs/nixos-24.05";
+    nixosStable.url = "nixpkgs/nixos-24.11";
     nixosHardware.url = "github:NixOS/nixos-hardware";
     nixDarwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixUtils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     homeManager = {
       url = "github:nix-community/home-manager";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        # utils.follows = "nixUtils";
       };
     };
 
+    # flakeUtils = {
+    #   url = "github:numtide/flake-utils";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    # flakeUtilsPlus.url = "github:gytis-ivaskevicius/flake-utils-plus";
+
     devshell = {
       url = "github:numtide/devshell";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        # flake-utils.follows = "nixUtils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nid = {
       url = "github:nix-community/nix-index-database";
@@ -43,118 +43,22 @@
   outputs =
     { self, ... }@inputs:
     let
-      # pkgs = self.pkgs.x86_64-linux.nixpkgs;
-      # mkApp = inputs.nixUtils.lib.mkApp;
       paths = import ./paths.nix;
       mkConfig = import paths.libraries.mkConf {
         inherit self inputs paths;
       };
-      # mkConfig =
-      #   name: extraArgs:
-      #   let
-      #     host =
-      #       let
-      #         inherit (inputs.nixpkgs.lib.lists) foldl' filter;
-      #         confCommon = import (paths.core.configurations.hosts + "/common");
-      #         confSystem = import (paths.core.configurations.hosts + "/${name}");
-      #         enabledUsers = map (user: user.name) (filter (user: user.enable or true) confSystem.people);
-      #         userConfigs = foldl' (
-      #           acc: userFile: acc // import (paths.core.configurations.users + "/${userFile}")
-      #         ) { } enabledUsers;
-      #       in
-      #       {
-      #         inherit name userConfigs;
-      #       }
-      #       // confCommon
-      #       // confSystem
-      #       // extraArgs;
-      #     specialModules =
-      #       let
-      #         inherit (host) desktop;
-      #         core =
-      #           (with paths.core; [
-      #             libraries
-      #             modules
-      #             options
-      #           ])
-      #           ++ (with inputs; [
-      #             stylix.nixosModules.stylix
-      #             nid.nixosModules.nix-index
-      #           ]);
-      #         home =
-      #           with inputs;
-      #           if desktop == "hyprland" then
-      #             [ ]
-      #           else if desktop == "plasma" then
-      #             [ plasmaManager.homeManagerModules.plasma-manager ]
-      #           else if desktop == "xfce" then
-      #             [ ]
-      #           else
-      #             [ ];
-      #       in
-      #       {
-      #         inherit core home;
-      #       };
-
-      #     specialArgs = {
-      #       inherit self paths host;
-      #       modules = specialModules;
-      #       libraries = import paths.libraries.store; # TODO: Check on this
-      #     };
-      #   in
-      #   import paths.libraries.mkCore {
-      #     inherit (inputs)
-      #       nixosStable
-      #       nixosUnstable
-      #       homeManager
-      #       nixDarwin
-      #       ;
-
-      #     inherit (host)
-      #       name
-      #       # system
-      #       ;
-
-      #     inherit
-      #       specialArgs
-      #       specialModules
-      #       ;
-
     in
-    #     system = host.platform;
-    #     preferredRepo = host.preferredRepo or "unstable";
-    #     allowUnfree = host.allowUnfree or true;
-    #     allowAliases = host.allowAliases or true;
-    #     allowHomeManager = host.allowHomeManager or true;
-    #     backupFileExtension = host.backupFileExtension or "BaC";
-    #     extraPkgConfig = host.extraPkgConfig or { };
-    #     extraPkgAttrs = host.extraPkgAttrs or { };
-    #   };
-    # inputs.nixUtils.lib.mkFlake {
-
-    #   supportedSystems = [
-    #     "aarch64-linux"
-    #     "x86_64-linux"
-    #   ];
-
-    #   channelsConfig = {
-    #     allowUnfree = true;
-    #   };
-
-    #   hosts = {
-    #     QBX = {
-    #       system = "x86_64-linux";
-    #       modules = [ ./configurations/hosts/qbx ];
-    #     };
-    #   };
-    # };
     {
-      inherit paths;
+      # inherit paths;
       nixosConfigurations = {
         example = mkConfig "example" { };
         preci = mkConfig "preci" { };
         dbook = mkConfig "dbook" { };
       };
+
+      # devShells.default = pkgs.mkShell {
+      #   inputsFrom = [ (import ./shell.nix { inherit pkgs; }) ];
+      # };
 
       #TODO: Create separate config directory for nix systems since the config is drastically different
       # darwinConfigurations = {
