89ab0bc7e73754f4a973049b71e075a682f368ee
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-04 22:34:34 -0500

    flake-utils-plus

--- Modules/nixos/configurations/hosts/qbx/flake.nix
+++ Modules/nixos/configurations/hosts/qbx/flake.nix
@@ -3,31 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils-plus = {
+      url = "github:gytis-ivaskevicius/flake-utils-plus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-    in
     {
-      qbx = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ ./. ];
+      self,
+      nixpkgs,
+      flake-utils-plus,
+    }:
+    flake-utils-plus.lib.mkFlake {
+      inherit self nixpkgs;
+
+      systems = [ "x86_64-linux" ];
+
+      nixosConfigurations = {
+        qbx = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./. ];
+        };
       };
 
-      devShells.${system}.default = nixpkgs.mkShell {
-        buildInputs = with nixpkgs; [
-          treefmt2
-          nixfmt-rfc-style
-          nixd
-          shfmt
-          shellcheck
-          bashInteractive
-        ];
+      devShells = {
+        x86_64-linux = {
+          default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+            buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+              treefmt2
+              nixfmt-rfc-style
+              nixd
+              shfmt
+              shellcheck
+              bashInteractive
+            ];
+          };
+        };
       };
 
-      referencedDevShell = self.devShells.x86_64-linux.default;
       sourceInfo = self.sourceInfo;
     };
 }
+  
