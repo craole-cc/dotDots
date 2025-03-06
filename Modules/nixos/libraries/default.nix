542abfa8b2c38ff9713765baffbc4312f2eb4ffe
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-05 22:44:22 -0500

    added dib

--- Modules/nixos/libraries/default.nix
+++ Modules/nixos/libraries/default.nix
@@ -1,11 +1,10 @@
-{ config, lib, ... }:
 {
   imports = [
     ./helpers.nix
     ./fetchers.nix
     ./filesystem.nix
     ./lists.nix
-  ];
 
-  options.dib = lib // config.DOTS.lib;
+    ./options.nix
+  ];
 }
--- Modules/nixos/libraries/options.nix
+++ Modules/nixos/libraries/options.nix
@@ -5,38 +5,61 @@
 }:
 let
   inherit (lib) mkOption;
-  inherit (config.DOTS) Libraries;
+  # inherit (config.DOTS.lib.helpers) mkHash;
+
+  inherit (config.DOTS.lib.filesystem)
+    importNixModules
+    importModules
+    locateFlake
+    locateGitRoot
+    locateNixos
+    locateParentByChild
+    locateParentByChildren
+    locateParentByName
+    locateParentByNameOrChildren
+    locateProjectRoot
+    pathOrNull
+    pathof
+    pathofGitHub
+    pathofPWD
+    pathsIgnored
+    pathsIgnoredCheck
+    pathsIn
+    ;
 in
 # with lib;
 # with types;
-with config.DOTS.Libraries;
-with types;
-with filesystem;
-with path;
+# with config.DOTS.lib;
+# with types;
+# with config.DOTS.lib.filesystem;
+with config.DOTS.lib.helpers;
 {
   options.dib = {
+    #| Helpers
+    mkHash = mkOption { default = mkHash; };
+
     #| Filesystem
-    importNixModules = mkOption { default = importNixModules; };
-    listFilesRecursively = mkOption { default = listFilesRecursively; };
-    listNixModules = mkOption { default = listNixModules; };
-    locateParentByName = mkOption { default = locateParentByName; };
-    locateParentByNameOrChildren = mkOption { default = locateParentByNameOrChildren; };
-    locateParentByChildren = mkOption { default = locateParentByChildren; };
-    locateProjectRoot = mkOption { default = locateProjectRoot; };
-    pathof = mkOption { default = pathof; };
+    # importNixModules = mkOption { default = importNixModules; };
+    # listFilesRecursively = mkOption { default = listFilesRecursively; };
+    # listNixModules = mkOption { default = listNixModules; };
+    # locateParentByName = mkOption { default = locateParentByName; };
+    # locateParentByNameOrChildren = mkOption { default = locateParentByNameOrChildren; };
+    # locateParentByChildren = mkOption { default = locateParentByChildren; };
+    # locateProjectRoot = mkOption { default = locateProjectRoot; };
+    # pathof = mkOption { default = pathof; };
 
     #| Path
     # listNixModuleNames = mkOption { default = listNixModuleNames; };
     # listNixModulePaths = mkOption { default = listNixModulePaths; };
     # listNixModulesRecursively = mkOption { default = listNixModulesRecursively; };
     # nixModulesWithOptions = mkOption { default = nixModulesWithOptions; };
-    listNixPackagesRecursively = mkOption { default = listNixPackagesRecursively; };
+    # listNixPackagesRecursively = mkOption { default = listNixPackagesRecursively; };
 
     #| Types
-    inFileList = mkOption { default = inFileList; };
-    isAllowedDir = mkOption { default = isAllowedDir; };
-    isNixFile = mkOption { default = isNixFile; };
-    inFolderList = mkOption { default = inFolderList; };
-    isGitIgnored = mkOption { default = isGitIgnored; };
+    # inFileList = mkOption { default = inFileList; };
+    # isAllowedDir = mkOption { default = isAllowedDir; };
+    # isNixFile = mkOption { default = isNixFile; };
+    # inFolderList = mkOption { default = inFolderList; };
+    # isGitIgnored = mkOption { default = isGitIgnored; };
   };
 }
