c6af227cd55dbca59a173a7e2e9fe7a40f28b040
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-06 09:11:02 -0500

    updating lib

--- Modules/nixos/libraries/fetchers.nix
+++ Modules/nixos/libraries/fetchers.nix
@@ -6,49 +6,57 @@
 }:
 let
   #| Native Imports
-  inherit (builtins)
-    getEnv
-    currentTime
-    ;
+  inherit (builtins) getEnv;
   inherit (pkgs) runCommand;
-  inherit (lib.strings)
-    fileContents
-    ;
+  inherit (lib.strings) fileContents;
   inherit (lib.options) mkOption;
   inherit (lib.types) str;
 
   #| Extended Imports
-  inherit (config) DOTS;
 
-  base = "lib";
+  #| Module Parts
+  dom = "lib";
   mod = "fetchers";
+
+  #| Module Options
+  currentUser = mkOption {
+    description = "Get the username of the current user.";
+    default =
+      let
+        viaEnvUSER = getEnv "USER";
+        viaUSERNAME = getEnv "USERNAME";
+        result = if viaEnvUSER != null then viaEnvUSER else viaUSERNAME;
+      in
+      result;
+    type = str;
+  };
+
+  currentTime = mkOption {
+    description = ''
+      Formatted time
+    '';
+    default =
+      let
+        viaDateCommand = fileContents (
+          runCommand "date" { } ''
+            date "+%Y-%m-%d %H:%M:%S %Z" > $out
+          ''
+        );
+      in
+      viaDateCommand;
+  };
+
+  #| Module Exports
+  exports = {
+    inherit
+      currentUser
+      currentTime
+      ;
+  };
 in
 {
-  options.DOTS.${base}.${mod} = {
-    currentUser = mkOption {
-      description = "Get the username of the current user.";
-      default =
-        let
-          viaEnvUSER = getEnv "USER";
-          viaUSERNAME = getEnv "USERNAME";
-          result = if viaEnvUSER != null then viaEnvUSER else viaUSERNAME;
-        in
-        result;
-      type = str;
-    };
-    currentTime = mkOption {
-      description = ''
-        Formatted time
-      '';
-      default =
-        let
-          viaDateCommand = fileContents (
-            runCommand "date" { } ''
-              date "+%Y-%m-%d %H:%M:%S %Z" > $out
-            ''
-          );
-        in
-        viaDateCommand;
-    };
+  options = {
+    DOTS.${dom}.${mod} = exports;
+    dib = exports;
   };
 }
