0bb0a818a1e542887059331495eab4cc8886b117
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-09 11:46:19 -0500

    moved paths to it's own file

--- flake.nix
+++ flake.nix
@@ -1,6 +1,61 @@
 {
   description = "NixOS Configuration Flake";
 
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    inputs.flakeUtils.lib.eachDefaultSystem (
+      system:
+      let
+        paths = import ./paths.nix;
+        mkConfig = import paths.libraries.mkConf {
+          inherit self inputs paths;
+        };
+      in
+      {
+        # systems = nixpkgs.lib.systems.flakeExposed;
+        # systems = [
+        #   "x86_64-linux"
+        #   "aarch64-linux"
+        # ];
+
+        devShells.default =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+              overlays = with inputs; [ flakeShell.overlays.default ];
+            };
+            inherit (pkgs.devshell) mkShell importTOML;
+          in
+          mkShell {
+            imports = with paths.devShells; [
+              (importTOML dots)
+              # (importTOML dev)
+              # (importTOML media)
+              # (importTOML env)
+            ];
+          };
+
+        nixosConfigurations = {
+          QBX = mkConfig "QBX" { };
+          Preci = mkConfig "Preci" { };
+          dbOOK = mkConfig "dbOOK" { };
+        };
+
+        #TODO: Create separate config directory for nix darwin systems since the config is drastically different
+        # darwinConfigurations = {
+        #   MBPoNine = mkDarwinConfig "MBPoNine" { };
+        # };
+
+        # TODO create mkHome for standalone home manager configs
+        # homeConfigurations = mkHomeConfig "craole" { };
+      }
+    );
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixosUnstable.url = "nixpkgs/nixos-unstable";
@@ -12,12 +67,8 @@
     };
     homeManager = {
       url = "github:nix-community/home-manager";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    # systems = "";
-
     flakeParts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -45,155 +96,4 @@
     };
     stylix.url = "github:danth/stylix";
   };
-
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flakeUtils,
-      flakeShell,
-      flakeFormatter,
-      ...
-    }@inputs:
-    flakeUtils.lib.eachDefaultSystem (
-      system:
-      let
-        # # Small tool to iterate over each systems
-        # eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-
-        # # Eval the treefmt modules from ./treefmt.nix
-        # treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-
-        paths =
-          let
-            flake = {
-              local = "/home/craole/.dots";
-              root = "/dots";
-              store = ./.;
-            };
-            parts = {
-              args = "/args";
-              cfgs = "/configurations";
-              env = "/environment";
-              libs = "/libraries";
-              mkCore = "/helpers/mkCoreConfig.nix";
-              mkConf = "/helpers/mkConfig.nix";
-              modules = "/Modules/nixos";
-              devShells = "/Modules/dev";
-              mods = "/modules";
-              opts = "/options";
-              pkgs = "/packages";
-              bin = "/Bin";
-              svcs = "/services";
-              ui = "/ui";
-              uiCore = "/ui/core";
-              uiHome = "/ui/home";
-              hosts = parts.cfgs + "/hosts";
-              users = parts.cfgs + "/users";
-              scripts = "/scripts";
-            };
-            devShells = rec {
-              default = flake.store + parts.devShells;
-              dots = default + "/dots.toml";
-              dev = default + "/dev.toml";
-              env = default + "/env.toml";
-              media = default + "/media.toml";
-            };
-            core = {
-              default = modules.store;
-              configurations = {
-                hosts = core.default + parts.hosts;
-                users = core.default + parts.users;
-              };
-              environment = core.default + parts.env;
-              libraries = core.default + parts.libs;
-              modules = core.default + parts.mods;
-              options = core.default + parts.opts;
-              packages = core.default + parts.pkgs;
-              services = core.default + parts.svcs;
-            };
-            home = {
-              default = modules.store + "/home";
-              configurations = home.default + parts.cfgs;
-              environment = home.default + parts.env;
-              libraries = home.default + parts.libs;
-              modules = home.default + parts.mods;
-              options = home.default + parts.opts;
-              packages = home.default + parts.pkgs;
-              services = home.default + parts.svcs;
-            };
-            scripts = {
-              global = flake.local + parts.bin;
-              local = modules.store + parts.scripts;
-              store = flake.store + parts.scripts;
-              dots = modules.store + parts.scripts + "/init_dots";
-            };
-            modules = {
-              local = flake.local + parts.modules;
-              store = flake.store + parts.modules;
-            };
-            libraries = {
-              local = modules.local + parts.libs;
-              store = modules.store + parts.libs;
-              mkCore = core.libraries + parts.mkCore;
-              mkConf = core.libraries + parts.mkConf;
-            };
-          in
-          {
-            inherit
-              flake
-              devShells
-              core
-              home
-              scripts
-              parts
-              modules
-              libraries
-              ;
-          };
-        mkConfig = import paths.libraries.mkConf {
-          inherit self inputs paths;
-        };
-      in
-      {
-        inherit paths;
-
-        devShells.default =
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ flakeShell.overlays.default ];
-            };
-
-            inherit (pkgs.devshell) mkShell importTOML;
-          in
-          mkShell {
-            imports = with paths.devShells; [
-              (importTOML dots)
-              # (importTOML dev)
-              # (importTOML media)
-              # (importTOML env)
-            ];
-          };
-
-        # formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
-
-        # checks = eachSystem (pkgs: {
-        #   formatting = treefmtEval.${pkgs.system}.config.build.check self;
-        # });
-        nixosConfigurations = {
-          QBX = mkConfig "QBX" { };
-          Preci = mkConfig "Preci" { };
-          # dbook = mkConfig "dbook" { };
-        };
-
-        #TODO: Create separate config directory for nix darwin systems since the config is drastically different
-        # darwinConfigurations = {
-        #   MBPoNine = mkDarwinConfig "MBPoNine" { };
-        # };
-
-        # TODO create mkHome for standalone home manager configs
-        # homeConfigurations = mkHomeConfig "craole" { };
-      }
-    );
 }
