21bf3871882ae01731e2f208c1404dc5876ebac4
Craole <32288735+Craole@users.noreply.github.com>
2025-03-10 10:33:45 -0500

    updated env.nix to use scripts

--- Modules/nixos/scripts/devshells/env.nix
+++ Modules/nixos/scripts/devshells/env.nix
@@ -1,92 +1,33 @@
 { pkgs, paths, ... }:
 {
-  # motd = ''
-  #   Welcome to the development environment!
-  # '';
-
-  packages = with pkgs; [
-    bat
-    btop
-    direnv
-    eza
-    fastfetch
-    fd
-    fzf
-    glib
-    jq
-    lsd
-    ripgrep
-    thefuck
-    lesspipe
-    yazi
-    zoxide
-    bashInteractive
-    fish
-    nushell
-    powershell
-    rbw
-    zsh
-    just
-  ];
   env = [
-    {
-      name = "HTTP_PORT";
-      value = 8080;
-    }
     {
       name = "PATH";
       prefix = "${paths.scripts.store.flake}";
     }
     {
-      name = "XDG_CACHE_DIR";
+      name = "CACHE_DIR";
       eval = "$PRJ_ROOT/.cache";
     }
-    {
-      name = "CARGO_HOME";
-      unset = true;
-    }
   ];
+
   commands = [
     {
       name = "rebuild";
       category = "System Management";
-      command = "sudo nixos-rebuild --flake $DOTS switch $@";
+      command = ''sudo nixos-rebuild --flake $DOTS switch $@'';
       help = "Rebuild NixOS with the changes made to the flake";
     }
     {
       name = "clean";
       category = "System Management";
-      command = "nix-collect-garbage --delete-old";
+      command = ''nix-collect-garbage --delete-old'';
       help = "Remove old nixos and home-manager generations";
     }
     {
       name = "update";
       category = "System Management";
-      command = ''
-        #!/bin/sh
-
-        #@ Ensure strict mode and exit on error
-        set -euo pipefail
-
-        #@ Pull repo changes, capturing output
-        PULL_OUTPUT=$(git pull --quiet --autostash 2>&1)
-        PULL_IGNORE="Already up to date."
-        case "$PULL_OUTPUT" in *"$PULL_IGNORE"*) ;;
-          *) printf "%s\n" "$PULL_OUTPUT" ;;
-        esac
-
-        #@ Update flake inputs
-        nix flake update
-
-        #@ Sync repo and push changes
-        git add --all :/
-        if git status --porcelain | grep -q .; then
-          msg="General updates"
-          [ $# -gt 0 ] && msg="$*"
-          git commit --message "$msg" || true
-          git push
-        fi
-      '';
+      command = ''update-repo.sh && update-flake.sh'';
       help = "Update flake inputs, sync repo, and push changes";
     }
 
@@ -127,10 +68,10 @@
       category = "Environment Management";
       package = "fzf";
     }
-    {
-      category = "Interactive Shell & Scripting";
-      package = "just";
-    }
+    # {
+    #   category = "Interactive Shell & Scripting";
+    #   package = "just";
+    # }
     {
       category = "Environment Management";
       package = "lsd";
@@ -145,14 +86,19 @@
       name = "pwsh";
       package = "powershell";
     }
-    {
-      category = "Environment Management";
-      package = "rbw";
-    }
+    # {
+    #   category = "Environment Management";
+    #   package = "rbw";
+    # }
     {
       category = "Information Management";
       name = "rg";
       package = "ripgrep";
+    }
+    {
+      category = "Interactive Shell & Scripting";
+      name = "treefmt";
+      package = "treefmt2";
     }
     {
       category = "Environment Management";
@@ -166,5 +112,12 @@
       category = "Interactive Shell & Scripting";
       package = "zsh";
     }
+  ];
+
+  packages = with pkgs; [
+    glib
+    jq
+    thefuck
+    # fish-lsp
   ];
 }
