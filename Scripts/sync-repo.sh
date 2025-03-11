32136b5e186ba41c861d50b6dc4555dc88248a68
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-11 12:24:01 -0500

    using exit in get_status

--- Scripts/sync-repo.sh
+++ Scripts/sync-repo.sh
@@ -8,9 +8,9 @@ main() {
     pull_updates
 
     # shellcheck disable=SC2310
-    get_status || return 0
+    get_status
     echo pop
-    
+
     # add_changes
     # commit_changes
     # push_changes
@@ -80,7 +80,7 @@ pull_updates() {
 
 get_status() {
     #@ Skip if there are no change
-    git status --porcelain >/dev/null 2>&1 && exit 0
+    git status --porcelain >/dev/null 2>&1 || exit 0
 
     #@ Display the current status
     git status --short
