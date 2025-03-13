0045de1a7c0eea2aa3d7230da7ea18bb3d42dfd6
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-13 10:27:59 -0500

    re-added pull options

--- Scripts/sync-repo.sh
+++ Scripts/sync-repo.sh
@@ -45,30 +45,30 @@ set_operation_mode() {
 
   #@ Enable trace, if requested
   case "${VERBOSITY_LEVEL}" in
-    "${VERBOSITY_LEVEL_TRACE}") set -x ;;
-    # "${VERBOSITY_LEVEL_DEBUG}") set -v ;;
-    *) ;;
+  "${VERBOSITY_LEVEL_TRACE}") set -x ;;
+  # "${VERBOSITY_LEVEL_DEBUG}") set -v ;;
+  *) ;;
   esac
 }
 
 parse_arguments() {
   while [ $# -ge 1 ]; do
     case "${1}" in
-      -t | --trace) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_TRACE}" ;;
-      -V | --verbose) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_INFO}" ;;
-      -d | --debug | --dry-run) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_DEBUG}" ;;
-      --info) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_INFO}" ;;
-      --warn*) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_WARN}" ;;
-      --error) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_ERROR}" ;;
-      -q | --quiet) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_QUIET}" ;;
-      -m | --message)
-        msg="${2}"
-        shift
-        ;;
-      -a | --amend) amend_commit="true" ;;
-      *)
-        msg="${msg}${msg:+${delimiter}}${1}"
-        ;;
+    -t | --trace) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_TRACE}" ;;
+    -V | --verbose) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_INFO}" ;;
+    -d | --debug | --dry-run) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_DEBUG}" ;;
+    --info) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_INFO}" ;;
+    --warn*) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_WARN}" ;;
+    --error) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_ERROR}" ;;
+    -q | --quiet) VERBOSITY_LEVEL="${VERBOSITY_LEVEL_QUIET}" ;;
+    -m | --message)
+      msg="${2}"
+      shift
+      ;;
+    -a | --amend) amend_commit="true" ;;
+    *)
+      msg="${msg}${msg:+${delimiter}}${1}"
+      ;;
     esac
     shift
   done
@@ -89,44 +89,44 @@ pout() {
   #@ Parse the arguments
   while [ "$#" -ge 1 ]; do
     case "${1}" in
-      --trace)
-        [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_TRACE}" ] || return 0
-        pout_tag="<\ TRACE />"
-        ;;
-      --debug)
-        [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_DEBUG}" ] || return 0
-        pout_tag="<\ DEBUG />"
-        ;;
-      --info)
-        [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_INFO}" ] || return 0
-        pout_tag="<\  INFO />"
-        ;;
-      --warn*)
-        [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_WARN}" ] || return 0
-        pout_tag="<\  WARN />"
-        ;;
-      --err*)
-        [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_ERROR}" ] || return 0
-        pout_tag="<\ ERROR />"
-        ;;
-      --upper)
-        case_mod="upper"
-        ;;
-      --lower)
-        case_mod="lower"
-        ;;
-      --sentence)
-        case_mod="sentence"
-        ;;
-      --title)
-        case_mod="title"
-        ;;
-      *)
-        case "${1}" in
-          '') ;;
-          *) pout_msg="${pout_msg}${pout_msg:+${delimiter}}${1}" ;;
-        esac
-        ;;
+    --trace)
+      [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_TRACE}" ] || return 0
+      pout_tag="<\ TRACE />"
+      ;;
+    --debug)
+      [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_DEBUG}" ] || return 0
+      pout_tag="<\ DEBUG />"
+      ;;
+    --info)
+      [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_INFO}" ] || return 0
+      pout_tag="<\  INFO />"
+      ;;
+    --warn*)
+      [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_WARN}" ] || return 0
+      pout_tag="<\  WARN />"
+      ;;
+    --err*)
+      [ "${VERBOSITY_LEVEL}" -ge "${VERBOSITY_LEVEL_ERROR}" ] || return 0
+      pout_tag="<\ ERROR />"
+      ;;
+    --upper)
+      case_mod="upper"
+      ;;
+    --lower)
+      case_mod="lower"
+      ;;
+    --sentence)
+      case_mod="sentence"
+      ;;
+    --title)
+      case_mod="title"
+      ;;
+    *)
+      case "${1}" in
+      '') ;;
+      *) pout_msg="${pout_msg}${pout_msg:+${delimiter}}${1}" ;;
+      esac
+      ;;
     esac
     shift
   done
@@ -138,34 +138,34 @@ pout() {
 
   #@ Modify case, if requested
   case "${case_mod}" in
-    upper) pout_msg="$(printf "%s" "${pout_msg}" | tr '[:lower:]' '[:upper:]')" ;;
-    lower) pout_msg="$(printf "%s" "${pout_msg}" | tr '[:upper:]' '[:lower:]')" ;;
-    sentence)
-      first_char=$(printf "%s" "${pout_msg}" | cut -c1 | tr '[:lower:]' '[:upper:]')
-      rest_of_string=$(printf "%s" "${pout_msg}" | cut -c2- | tr '[:upper:]' '[:lower:]')
-      pout_msg="${first_char}${rest_of_string}."
-      ;;
-    title)
-      pout_msg="$(pout --lower "${pout_msg}")"
-      words=$(printf "%s" "${pout_msg}")
-      title_cased=""
+  upper) pout_msg="$(printf "%s" "${pout_msg}" | tr '[:lower:]' '[:upper:]')" ;;
+  lower) pout_msg="$(printf "%s" "${pout_msg}" | tr '[:upper:]' '[:lower:]')" ;;
+  sentence)
+    first_char=$(printf "%s" "${pout_msg}" | cut -c1 | tr '[:lower:]' '[:upper:]')
+    rest_of_string=$(printf "%s" "${pout_msg}" | cut -c2- | tr '[:upper:]' '[:lower:]')
+    pout_msg="${first_char}${rest_of_string}."
+    ;;
+  title)
+    pout_msg="$(pout --lower "${pout_msg}")"
+    words=$(printf "%s" "${pout_msg}")
+    title_cased=""
 
-      for word in ${words}; do
-        first_char=$(printf "%s" "${word}" | cut -c1)
-        rest_of_word=$(printf "%s" "${word}" | cut -c2-)
-        first_char_upper="$(pout --upper "${first_char}")"
+    for word in ${words}; do
+      first_char=$(printf "%s" "${word}" | cut -c1)
+      rest_of_word=$(printf "%s" "${word}" | cut -c2-)
+      first_char_upper="$(pout --upper "${first_char}")"
 
-        # Only add space if not the first word
-        if [ -n "${title_cased}" ]; then
-          title_cased="${title_cased} "
-        fi
-        title_cased="${title_cased}${first_char_upper}${rest_of_word}"
-      done
+      # Only add space if not the first word
+      if [ -n "${title_cased}" ]; then
+        title_cased="${title_cased} "
+      fi
+      title_cased="${title_cased}${first_char_upper}${rest_of_word}"
+    done
 
-      # Define the message as the whole title-cased string
-      pout_msg="${title_cased}"
-      ;;
-    *) ;;
+    # Define the message as the whole title-cased string
+    pout_msg="${title_cased}"
+    ;;
+  *) ;;
   esac
 
   #@ Print the tag if it exists, followed bu a space
@@ -190,7 +190,7 @@ validate_git() {
   fi
 
   #@ Attempt to retrieve the path to the project root directory
-  git_dir="$(eval "${GIT_CMD} rev-parse --show-toplevel" 2> /dev/null)" || {
+  git_dir="$(eval "${GIT_CMD} rev-parse --show-toplevel" 2>/dev/null)" || {
     pout --error "This directory is not part of a git repository."
     pout --warn "Please navigate to a valid git repository and try again."
     return 1
@@ -206,7 +206,7 @@ validate_git() {
 
 create_cmd_output_file() {
   #@ Create a temporary file if possible
-  CMD_OUTPUT=$(mktemp 2> /dev/null || mktemp -t "githelper.XXXXXX")
+  CMD_OUTPUT=$(mktemp 2>/dev/null || mktemp -t "githelper.XXXXXX")
   if [ -z "${CMD_OUTPUT}" ] || [ ! -f "${CMD_OUTPUT}" ]; then
     pout --error "Failed to create temporary file"
     return 1
@@ -235,28 +235,28 @@ create_cmd_output_file() {
     #@ Parse arguments
     while [ $# -ge 1 ]; do
       case "${1}" in
-        --cmd | --command)
-          CMD="${2}"
-          ;;
-        --header-only | --no-exec)
-          header_print=only
-          ;;
-        --no-header)
-          header_print=off
-          ;;
-        --success) CMD_SUCCESS="${2}" ;;
-        --failure) CMD_FAILURE="${2}" ;;
-        --error | --info | --warn | --debug | quiet | --trace)
-          CMD_TAG="${1}"
-          CMD_MSG="${2}"
-          ;;
-        --label)
-          CMD_LABEL="${2}"
-          ;;
-        --no-tag)
-          CMD_TAG=""
-          ;;
-        *) ;;
+      --cmd | --command)
+        CMD="${2}"
+        ;;
+      --header-only | --no-exec)
+        header_print=only
+        ;;
+      --no-header)
+        header_print=off
+        ;;
+      --success) CMD_SUCCESS="${2}" ;;
+      --failure) CMD_FAILURE="${2}" ;;
+      --error | --info | --warn | --debug | quiet | --trace)
+        CMD_TAG="${1}"
+        CMD_MSG="${2}"
+        ;;
+      --label)
+        CMD_LABEL="${2}"
+        ;;
+      --no-tag)
+        CMD_TAG=""
+        ;;
+      *) ;;
       esac
       shift
     done
@@ -271,11 +271,11 @@ create_cmd_output_file() {
     CMD_LABEL="$(pout --title "${CMD_LABEL}")"
 
     case "${header_print}" in
-      no | off) ;;
-      *)
-        pout --info "===" "${CMD_LABEL}" "==="
-        pout --debug "Command:" "${CMD}"
-        ;;
+    no | off) ;;
+    *)
+      pout --info "===" "${CMD_LABEL}" "==="
+      pout --debug "Command:" "${CMD}"
+      ;;
     esac
 
     if [ "${header_print}" = "only" ]; then
@@ -284,7 +284,7 @@ create_cmd_output_file() {
 
     if [ -n "${CMD_TAG}" ]; then
       #@ Capturing the status command through redirection
-      if eval "${CMD}" > "${CMD_OUTPUT}" 2>&1; then
+      if eval "${CMD}" >"${CMD_OUTPUT}" 2>&1; then
         #TODO: <\ ERROR /> /home/craole/.dots/Scripts/sync-repo.sh: eval: line 285: unexpected EOF while looking for matching `''
         #TODO: <\ ERROR /> Failed to amend the last commit. [Exit Code: 2]
         CMD_STATUS="$?"
@@ -302,10 +302,10 @@ create_cmd_output_file() {
 
     #@ Update the status message to account for debug mode
     case "${VERBOSITY_LEVEL}" in
-      "${VERBOSITY_LEVEL_TRACE}" | "${VERBOSITY_LEVEL_DEBUG}")
-        msg_debug="would have"
-        ;;
-      *) msg_debug="" ;;
+    "${VERBOSITY_LEVEL_TRACE}" | "${VERBOSITY_LEVEL_DEBUG}")
+      msg_debug="would have"
+      ;;
+    *) msg_debug="" ;;
     esac
     CMD_SUCCESS="$(pout --sentence "${msg_debug}" "${CMD_SUCCESS}")"
     CMD_FAILURE="$(pout --sentence "${msg_debug}" "${CMD_FAILURE}")"
@@ -315,28 +315,28 @@ create_cmd_output_file() {
 
       #@ Skip it "Already up to date"
       case "${CMD_RESULT}" in
-        *"Already up to date"*) pout --info "The local repo already in sync with the remote" ;;
-        *)
-          #@ Tag each line of output seperatly
-          while IFS= read -r line; do
-            case "${line}" in
-              '') ;;
-              *) pout --info "${line:-}" ;;
-            esac
-          done < "${CMD_OUTPUT}"
+      *"Already up to date"*) pout --info "The local repo already in sync with the remote" ;;
+      *)
+        #@ Tag each line of output seperatly
+        while IFS= read -r line; do
+          case "${line}" in
+          '') ;;
+          *) pout --info "${line:-}" ;;
+          esac
+        done <"${CMD_OUTPUT}"
 
-          #@ Print the status message
-          pout --info "${CMD_SUCCESS}"
-          ;;
+        #@ Print the status message
+        pout --info "${CMD_SUCCESS}"
+        ;;
       esac
     else
       #@ Tag each line of output seperatly
       while IFS= read -r line; do
         case "${line}" in
-          '') ;;
-          *) pout --error "${line:-}" ;;
+        '') ;;
+        *) pout --error "${line:-}" ;;
         esac
-      done < "${CMD_OUTPUT}"
+      done <"${CMD_OUTPUT}"
 
       #@ Print the status message
       pout --error "${CMD_FAILURE}" "[Exit Code: ${CMD_STATUS}]"
@@ -349,8 +349,8 @@ create_cmd_output_file() {
 
 pull_updates() {
   #@ Define command information
-  # cmd="${GIT_CMD} pull --autostash --no-rebase"
-  cmd="${GIT_CMD} pull"
+  cmd="${GIT_CMD} pull --autostash --no-rebase"
+  # cmd="${GIT_CMD} pull"
   cmd_label="Remote Update Retrieval"
   msg_success="Integrated remote changes with the local branch"
   msg_failure="Failed to pull updates from remote repository"
@@ -370,16 +370,16 @@ pull_updates() {
 
   #@ Update the command based on verbosity level
   case "${VERBOSITY_LEVEL}" in
-    "${VERBOSITY_LEVEL_DEBUG}")
-      cmd="${cmd} --dry-run"
-      ;;
-    "${VERBOSITY_LEVEL_TRACE}")
-      cmd="${cmd} --verbose"
-      ;;
-    "${VERBOSITY_LEVEL_QUIET}")
-      cmd="${cmd} --quiet"
-      ;;
-    *) ;;
+  "${VERBOSITY_LEVEL_DEBUG}")
+    cmd="${cmd} --dry-run"
+    ;;
+  "${VERBOSITY_LEVEL_TRACE}")
+    cmd="${cmd} --verbose"
+    ;;
+  "${VERBOSITY_LEVEL_QUIET}")
+    cmd="${cmd} --quiet"
+    ;;
+  *) ;;
   esac
 
   #@ Execute the command with necessay options
@@ -392,7 +392,7 @@ pull_updates() {
 
 get_status() {
   #@ Check if there are any changes to commit
-  _changes="$(${GIT_CMD} status --porcelain 2> /dev/null)"
+  _changes="$(${GIT_CMD} status --porcelain 2>/dev/null)"
 
   #@ Update the nothing_to_commit flag if there are no changes
   [ -n "${_changes}" ] || {
@@ -416,19 +416,19 @@ get_status() {
   #@ Define the command
   cmd="${GIT_CMD} status"
   case "${VERBOSITY_LEVEL}" in
-    "${VERBOSITY_LEVEL_TRACE}")
-      cmd="${cmd} --verbose"
-      ;;
-    "${VERBOSITY_LEVEL_DEBUG}")
-      cmd="${cmd} --long"
-      ;;
-    "${VERBOSITY_LEVEL_WARN}" | "${VERBOSITY_LEVEL_INFO}")
-      cmd="${cmd} --short"
-      ;;
-    "${VERBOSITY_LEVEL_ERROR}" | "${VERBOSITY_LEVEL_QUIET}")
-      cmd="${cmd} >/dev/null 2>&1"
-      ;;
-    *) ;;
+  "${VERBOSITY_LEVEL_TRACE}")
+    cmd="${cmd} --verbose"
+    ;;
+  "${VERBOSITY_LEVEL_DEBUG}")
+    cmd="${cmd} --long"
+    ;;
+  "${VERBOSITY_LEVEL_WARN}" | "${VERBOSITY_LEVEL_INFO}")
+    cmd="${cmd} --short"
+    ;;
+  "${VERBOSITY_LEVEL_ERROR}" | "${VERBOSITY_LEVEL_QUIET}")
+    cmd="${cmd} >/dev/null 2>&1"
+    ;;
+  *) ;;
   esac
 
   #@ Execute the command with necessay options
@@ -477,13 +477,13 @@ update_index() {
 
   #@ Update the command based on verbosity level
   case "${VERBOSITY_LEVEL}" in
-    "${VERBOSITY_LEVEL_INFO}")
-      # cmd="${cmd} --verbose"
-      ;;
-    "${VERBOSITY_LEVEL_TRACE}" | "${VERBOSITY_LEVEL_DEBUG}")
-      cmd="${cmd} --dry-run"
-      ;;
-    *) ;;
+  "${VERBOSITY_LEVEL_INFO}")
+    # cmd="${cmd} --verbose"
+    ;;
+  "${VERBOSITY_LEVEL_TRACE}" | "${VERBOSITY_LEVEL_DEBUG}")
+    cmd="${cmd} --dry-run"
+    ;;
+  *) ;;
   esac
 
   #@ Execute the command with necessay options
@@ -528,7 +528,7 @@ commit_changes() {
     --header-only
 
   #@ Retrive the most recent commit message
-  msg_last_commit="$(${GIT_CMD} log -1 --pretty=%B 2> /dev/null | tr -d '\n')"
+  msg_last_commit="$(${GIT_CMD} log -1 --pretty=%B 2>/dev/null | tr -d '\n')"
 
   #@ Define the commit message with a default value
   msg_init_commit="Initial commit"
@@ -548,11 +548,11 @@ commit_changes() {
 
   #@ Update the command based on verbosity level
   case "${VERBOSITY_LEVEL}" in
-    "${VERBOSITY_LEVEL_INFO}") cmd="${cmd} --verbose" ;;
-    "${VERBOSITY_LEVEL_TRACE}") cmd="${cmd} --dry-run --long" ;;
-    "${VERBOSITY_LEVEL_DEBUG}") cmd="${cmd} --dry-run --short" ;;
-    "${VERBOSITY_LEVEL_QUIET}") cmd="${cmd} --quiet" ;;
-    *) ;;
+  "${VERBOSITY_LEVEL_INFO}") cmd="${cmd} --verbose" ;;
+  "${VERBOSITY_LEVEL_TRACE}") cmd="${cmd} --dry-run --long" ;;
+  "${VERBOSITY_LEVEL_DEBUG}") cmd="${cmd} --dry-run --short" ;;
+  "${VERBOSITY_LEVEL_QUIET}") cmd="${cmd} --quiet" ;;
+  *) ;;
   esac
   cmd="${cmd} --message='${msg_commit}'"
 
@@ -592,18 +592,18 @@ push_changes() {
 
   #@ Update the command based on verbosity level
   case "${VERBOSITY_LEVEL}" in
-    "${VERBOSITY_LEVEL_TRACE}" | "${VERBOSITY_LEVEL_DEBUG}")
-      cmd="${cmd} --dry-run"
-      ;;
-    "${VERBOSITY_LEVEL_INFO}")
-      cmd="${cmd} --verbose"
-      ;;
-    # "${VERBOSITY_LEVEL_WARN}") ;; #TODO: Update to show only warnings
-    # "${VERBOSITY_LEVEL_ERROR}") ;;   #TODO: Update to show only errors
-    "${VERBOSITY_LEVEL_QUIET}")
-      cmd="${cmd} --quiet"
-      ;;
-    *) ;;
+  "${VERBOSITY_LEVEL_TRACE}" | "${VERBOSITY_LEVEL_DEBUG}")
+    cmd="${cmd} --dry-run"
+    ;;
+  "${VERBOSITY_LEVEL_INFO}")
+    cmd="${cmd} --verbose"
+    ;;
+  # "${VERBOSITY_LEVEL_WARN}") ;; #TODO: Update to show only warnings
+  # "${VERBOSITY_LEVEL_ERROR}") ;;   #TODO: Update to show only errors
+  "${VERBOSITY_LEVEL_QUIET}")
+    cmd="${cmd} --quiet"
+    ;;
+  *) ;;
   esac
 
   #@ Execute the command with necessay options
