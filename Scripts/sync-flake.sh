a238a40ba6aed43a4c2269ad2e087cb3b5f658c8
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-17 15:21:40 -0500

    readded editorconfig, will it work?

--- Scripts/sync-flake.sh
+++ Scripts/sync-flake.sh
@@ -1,48 +1,49 @@
 #!/bin/sh
 
 main() {
-    # set -eu
-    parse_arguments "$@"
-    update_repo
-    update_flake
-    update_repo
-}
-
-update_repo(){
-    command -v sync-repo.sh && sync-repo.sh --quiet "Flake Update"
+  # set -eu
+  parse_arguments "$@"
+  update_repo
+  update_flake
+  update_repo
 }
 
 parse_arguments() {
-    #@ Set defaults
-    delimiter=" "
-    args=""
+  #@ Set defaults
+  delimiter=" "
+  args=""
 
-    #@ Parse arguments
-    while [ $# -gt 0 ]; do
-        case "${1}" in
-        -a | --arg*)
-            args="${args}${args:+${delimiter}}${2}"
-            shift
-            ;;
-        *)
-            args="${args}${args:+${delimiter}}${1}"
-            ;;
-        esac
+  #@ Parse arguments
+  while [ $# -gt 0 ]; do
+    case "${1}" in
+      -a | --arg*)
+        args="${args}${args:+${delimiter}}${2}"
         shift
-    done
+        ;;
+      *)
+        args="${args}${args:+${delimiter}}${1}"
+        ;;
+    esac
+    shift
+  done
+}
+
+update_repo() {
+  command -v sync-repo.sh \
+    && sync-repo.sh --quiet "Flake Update"
 }
 
 update_flake() {
-    #@ Proceed only if nix flake is available
-    nix_flake="$(nix flake --help 2>/dev/null)"
-    [ -z "${nix_flake}" ] && {
-        printf "Nix flake doesn't seem to be available\n"
-        return 1
-    }
+  #@ Proceed only if nix flake is available
+  nix_flake="$(nix flake --help 2> /dev/null)"
+  [ -z "${nix_flake}" ] && {
+    printf "Nix flake doesn't seem to be available\n"
+    return 1
+  }
 
-    eval nix flake update "${args}"
-    eval nix flake archive
-    return 0
+  eval nix flake update "${args}"
+  eval nix flake archive
+  return 0
 }
 
 main "$@"
