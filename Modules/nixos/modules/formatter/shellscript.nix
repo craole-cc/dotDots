92487a8aa14ad21e8e00f529db3733baea1ebac1
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-28 05:18:14 +0000

    shfmt options

--- Modules/nixos/modules/formatter/shellscript.nix
+++ Modules/nixos/modules/formatter/shellscript.nix
@@ -56,7 +56,7 @@
               "--binary-next-line"
               "--space-redirects"
               "--case-indent"
-              "--indent-size=4"
+              "--indent-size" "4"
             ];
           };
         };
