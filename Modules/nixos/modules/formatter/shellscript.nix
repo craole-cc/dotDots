05f05d08830d77e1a03d6b103b0435e54e73b85c
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-03-28 05:18:56 +0000

    shfmt options

--- Modules/nixos/modules/formatter/shellscript.nix
+++ Modules/nixos/modules/formatter/shellscript.nix
@@ -50,14 +50,13 @@
           # shellcheck = { inherit includes excludes; };
           shfmt = {
             inherit includes excludes;
-            # indent_size = 4;
             options = [
               "--apply-ignore"
               "--binary-next-line"
               "--space-redirects"
               "--case-indent"
               "--indent-size"
-              "4"
+              "2"
             ];
           };
         };
