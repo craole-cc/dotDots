5ecf6b353d4e66a10f2d40b5615315770be317b7
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-01 04:42:57 +0000

    moved apps to home-manager instead

--- Modules/nixos/configurations/hosts/QBXL/core/env.nix
+++ Modules/nixos/configurations/hosts/QBXL/core/env.nix
@@ -20,67 +20,20 @@ in
           #@ Exit immediately if any command fails
           set -e
 
-          printf "NixOS WSL Flake for QBXL [%s]\n" "$QBXL"
+          printf "NixOS WSL Flake for QBXL [%s]\n" "${flake}"
 
           printf "/> Updating />\n"
-          nix flake update --flake "$QBXL"
+          nix flake update --flake "${flake}"
 
           printf "/> Committing />\n"
           gitui || true
 
           printf "/> Rebuilding />\n"
-          sudo nixos-rebuild switch --flake "$QBXL" --show-trace --upgrade
+          sudo nixos-rebuild switch --flake "${flake}" --show-trace --upgrade
         ''
       ))
-      alejandra
-      curl
-      devenv
-      fd
-      fzf
       gitui
-      helix
-      jq
-      nil
-      # nix-index
-      nixd
-      nixfmt-rfc-style
-      ripgrep
-      sd
-      shellcheck
-      shfmt
-      tldr
-      tokei
-      undollar
-      wget
     ];
   };
-  programs = {
-    bat.enable = true;
-    direnv = {
-      enable = true;
-      silent = true;
-    };
-    git = {
-      enable = true;
-      lfs.enable = true;
-      prompt.enable = true;
-      config = {
-        init = {
-          defaultBranch = "main";
-        };
-        url = {
-          "https://github.com/" = {
-            insteadOf = [
-              "gh:"
-              "github:"
-            ];
-          };
-        };
-      };
-    };
-    nix-ld.enable = true;
-    starship.enable = true;
-    vivid.enable = true;
-    yazi.enable = true;
-  };
+  programs.git.enable = true;
 }
--- Modules/nixos/configurations/hosts/QBXL/home/packages/default.nix
+++ Modules/nixos/configurations/hosts/QBXL/home/packages/default.nix
@@ -1,16 +1,72 @@
 { pkgs, osConfig, ... }:
 {
-  imports = [ ];
+  imports = [
+    ./bat
+    ./helix
+  ];
   home = {
+    sessionVariables = {
+      EDITOR = "hx";
+      VISUAL = "code-insiders";
+    };
     stateVersion = osConfig.system.stateVersion;
-    packages = with pkgs; [ cowsay ];
+    packages = with pkgs; [
+      alejandra
+      curl
+      devenv
+      fd
+      fzf
+      gitui
+      # helix
+      jq
+      nil
+      # nix-index
+      nixd
+      nixfmt-rfc-style
+      ripgrep
+      sd
+      shellcheck
+      shfmt
+      tldr
+      tokei
+      undollar
+      cowsay
+      wget
+    ];
   };
   programs = {
-    home-manager.enable = true;
     atuin = {
       enable = true;
       daemon.enable = true;
       enableBashIntegration = true;
     };
+    # bat.enable = true;
+    direnv = {
+      enable = true;
+      silent = true;
+    };
+    git = {
+      enable = true;
+      lfs.enable = true;
+      prompt.enable = true;
+      config = {
+        init = {
+          defaultBranch = "main";
+        };
+        url = {
+          "https://github.com/" = {
+            insteadOf = [
+              "gh:"
+              "github:"
+            ];
+          };
+        };
+      };
+    };
+    home-manager.enable = true;
+    nix-ld.enable = true;
+    starship.enable = true;
+    vivid.enable = true;
+    yazi.enable = true;
   };
 }
