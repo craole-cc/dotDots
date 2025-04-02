5081e6131226ef561a16e8113f0eae2e88ac7bd9
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-02 04:38:46 +0000

    Is this really working?

--- Modules/nixos/packages/custom/default.nix
+++ Modules/nixos/packages/custom/default.nix
@@ -6,18 +6,19 @@
 {
   aider-chat-env = pkgs.callPackage ./aider { };
   lyrics = pkgs.python3Packages.callPackage ./lyrics { };
-  # devshell = pkgs.callPackage ./devshell {
+  lyrics = pkgs.callPackage ./devshell { };
+  devshell = pkgs.callPackage ./devshell {
   #   inherit paths;
   #   inherit (pkgs.inputs.developmentShell) mkShell;
   # };
 
-  devshell =
-    if paths == null then
-      null
-    else
-      pkgs.callPackage ./devshell {
-        # inherit paths;
-        # inherit (pkgs.inputs.developmentShell) mkShell;
-      };
+  # devshell =
+  #   if paths == null then
+  #     null
+  #   else
+  #     pkgs.callPackage ./devshell {
+  #       # inherit paths;
+  #       # inherit (pkgs.inputs.developmentShell) mkShell;
+  #     };
 
 }
