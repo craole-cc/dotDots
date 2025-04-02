26ea63c26268a486d4365c02de3561930b09707a
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-02 04:15:52 +0000

    simplifying the devshell package call

--- Modules/nixos/packages/custom/default.nix
+++ Modules/nixos/packages/custom/default.nix
@@ -1,9 +1,6 @@
-{
-  pkgs ? import <nixpkgs> { },
-  ...
-}:
+{ pkgs, paths, ... }:
 {
   aider-chat-env = pkgs.callPackage ./aider { };
   lyrics = pkgs.python3Packages.callPackage ./lyrics { };
-  devshell = pkgs.callPackage ./devshell { };
+  devshell = pkgs.callPackage ./devshell { inherit paths; };
 }
