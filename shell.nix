bc1273595cd09b009e3c7ffb64ce0b730f51f4e0
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-03 23:10:20 +0000

    trying to get shell.nix to work

--- shell.nix
+++ shell.nix
@@ -1,22 +1,9 @@
 {
-  pkgs ? import <nixpkgs> { },
-  ...
+  pkgs ? import <nixpkgs> { }
 }:
 {
   default = pkgs.mkShell {
-    # NIX_CONFIG = "extra-experimental-features = nix-command flakes ca-derivations";
     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [
-      gh
-      git
-      helix
-      # sops
-      # ssh-to-age
-      # gnupg
-      # age
-
-    ];
+    shellHook = "exec nix develop";
   };
 }
-
-#TODO: This should simply enable flakes then run nix develop
