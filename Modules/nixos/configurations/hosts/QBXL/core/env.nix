50c22f64a70125dbc41edd8555a89377c173dd29
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-03 05:24:30 +0000

    choosing to fo with extraInit to init the .dotsrc and add bins to path there

--- Modules/nixos/configurations/hosts/QBXL/core/env.nix
+++ Modules/nixos/configurations/hosts/QBXL/core/env.nix
@@ -1,41 +1,37 @@
 {
-  pkgs,
   paths,
   config,
   dots,
   ...
 }:
-let
-  inherit (pkgs) writeShellScriptBin;
-  inherit (paths) flake parts;
-  inherit (parts.bin) shellscript;
-  local = flake.${config.networking.hostName};
-  # dotsScript = local + shellscript + "/project/.dots";
-  devScript = local + shellscript + "/project/nix/devnix";
-  edaScript = local + shellscript + "/packages/alias/edita";
-in
 {
-  environment = {
-    inherit (dots) shellAliases;
-    variables = dots.variables // {
-      DOTS = local;
+  environment = with dots.environment; {
+    inherit shellAliases extraInit;
+    variables = variables // {
+      DOTS = paths.flake.${config.networking.hostName};
     };
-    # TODO: Add the bins to PATH
-
-    systemPackages = [
-      (writeShellScriptBin "dotshell" ''
-        dev "${local}"
-      '')
-      (writeShellScriptBin "dev" ''
-        set -e
-        chmod +x "${devScript}"
-        "${devScript}" "$@"
-      '')
-      (writeShellScriptBin "eda" ''
-        set -e
-        chmod +x "${edaScript}"
-        "${edaScript}" "$@"
-      '')
-    ];
   };
+  # environment = {
+  #   inherit (dots) shellAliases;
+  #   variables = dots.variables // {
+  #     DOTS = local;
+  #   };
+  #   # TODO: Add the bins to PATH
+
+  #   systemPackages = [
+  #     (writeShellScriptBin "dotshell" ''
+  #       dev "${local}"
+  #     '')
+  #     (writeShellScriptBin "dev" ''
+  #       set -e
+  #       chmod +x "${devScript}"
+  #       "${devScript}" "$@"
+  #     '')
+  #     (writeShellScriptBin "eda" ''
+  #       set -e
+  #       chmod +x "${edaScript}"
+  #       "${edaScript}" "$@"
+  #     '')
+  #   ];
+  # };
 }
