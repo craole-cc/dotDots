6d14f5071bb04edb04b29117a5e6245720637573
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-04 15:05:18 +0000

    added location

--- Modules/nixos/configurations/users/craole/core/location.nix
+++ Modules/nixos/configurations/users/craole/core/location.nix
@@ -5,4 +5,16 @@
   };
   time.timeZone = "America/Jamaica";
   i18n.defaultLocale = "en_US.UTF-8";
+
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1";
+      night = "0.75";
+    };
+    temperature = {
+      day = 5500;
+      night = 3800;
+    };
+  };
 }
