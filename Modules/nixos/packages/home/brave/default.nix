969133d7f9f27f21bd803cdcd297792bc4b860d8
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-04-04 00:28:22 +0000

    brave already had options

--- Modules/nixos/packages/home/brave/default.nix
+++ Modules/nixos/packages/home/brave/default.nix
@@ -1,21 +1,5 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+  programs.brave = {
 
-let
-  inherit (lib.options) mkEnableOption;
-  inherit (lib.modules) mkIf;
-  cfg = config.programs.brave;
-in
-{
-  options.programs.brave = {
-    enable = mkEnableOption "Brave Browser";
-  };
-
-  config = mkIf cfg.enable {
-    home.packages = [ pkgs.brave ];
   };
 }
