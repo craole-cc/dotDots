09c929bcd135969cb7ea8194157b48116ffb432e
Craole <32288735+Craole@users.noreply.github.com>
2025-08-03 20:23:32 -0500

    skipped changing to $DOTS if in vscode

--- Configuration/powershell/profile.psm1
+++ Configuration/powershell/profile.psm1
@@ -33,11 +33,15 @@ Export-ModuleMember -Function Import-ProfileScript
 Import-ProfileScript -ScriptName 'utils.ps1' -Description 'utilities'
 Import-ProfileScript -ScriptName 'config.ps1' -Description 'configuration'
 
+# Set starting directory to DOTS (but not when in VSCode)
+$isVSCode = $env:VSCODE_PID -or $env:TERM_PROGRAM -eq "vscode" -or $env:VSCODE_INJECTION -eq "1"
 
-# Set starting directory to DOTS
-if ($env:DOTS -and (Test-Path $env:DOTS)) {
+if ($env:DOTS -and (Test-Path $env:DOTS) -and -not $isVSCode) {
   Set-Location $env:DOTS
   Write-Debug "Changed directory to: $env:DOTS"
+}
+elseif ($isVSCode) {
+  Write-Debug "VSCode detected - staying in current workspace directory"
 }
 else {
   Write-Warning "DOTS environment variable not set or path does not exist: $env:DOTS"
