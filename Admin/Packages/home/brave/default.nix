f77b24ad71185b4b0e565426fa889f69038fefe9
Craole <32288735+Craole@users.noreply.github.com>
2025-11-17 10:38:54 -0500

    Added keyboad settings

--- Admin/Packages/home/brave/default.nix
+++ Admin/Packages/home/brave/default.nix
@@ -1,4 +1,27 @@
 {
-  programs.brave = {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf;
+  cfg = config.programs.brave;
+in {
+  options.programs.brave = {
+    enable = mkEnableOption "Brave Browser";
+  };
+
+  imports = [
+    ./settings.nix
+    ./extensions.nix
+    ];
+
+  config = mkIf cfg.enable {
+    # home.packages = [pkgs.brave];
+    programs.chromium = {
+      enable = true;
+      package = pkgs.brave
+    };
   };
 }
