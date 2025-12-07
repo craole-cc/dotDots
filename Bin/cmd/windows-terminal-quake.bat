2e46a96a90ef3e9c84f7b87e80405df7f487bfab
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-07 12:16:32 -0500

    removed nixWSL from wt startup script

--- Bin/cmd/windows-terminal-quake.bat
+++ Bin/cmd/windows-terminal-quake.bat
@@ -1,1 +1,1 @@
-start wt.exe --window _quake; new-tab --profile "Git Bash"; new-tab --profile "NixOS"; focus-tab --target 0
+start wt.exe --window _quake; new-tab --profile "Git Bash"; focus-tab --target 0
