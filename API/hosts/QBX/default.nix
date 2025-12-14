155ec565b5582f7f7b2cf80d49f435fddba6b0ae
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-14 13:49:02 -0500

    Developing flake

--- API/hosts/QBX/default.nix
+++ API/hosts/QBX/default.nix
@@ -100,8 +100,8 @@ in {
   };
 
   localization = {
-    latitude = 18.015;
-    longitude = 77.49;
+    # latitude = 18.015;
+    # longitude = 77.49;
     timeZone = "America/Jamaica";
     defaultLocale = "en_US.UTF-8";
   };
