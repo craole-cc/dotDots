4cfe10311a9593f59f839ca6bcacd74fdec2d9c8
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-14 14:13:21 -0500

    Developing flake

--- Libraries/generators/nixos.nix
+++ Libraries/generators/nixos.nix
@@ -285,8 +285,8 @@
 
         #> Enable shells in home-manager
         programs = {
-          starship.enable = hasInfix "starship" (cfg.interface.prompt or "");
-          oh-my-posh.enable = hasInfix "posh" (cfg.interface.prompt or "");
+          # starship.enable = hasInfix "starship" (cfg.interface.prompt or "");
+          # oh-my-posh.enable = hasInfix "posh" (cfg.interface.prompt or "");
           bash.enable = elem "bash" (cfg.shells or []);
           zsh.enable = elem "zsh" (cfg.shells or []);
           fish.enable = elem "fish" (cfg.shells or []);
