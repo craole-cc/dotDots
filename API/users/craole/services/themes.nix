38b5edec4b3ad28b708b3355e559e73c2c4349f7
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-15 10:09:07 -0500

    Developing flake

--- API/users/craole/services/themes.nix
+++ API/users/craole/services/themes.nix
@@ -1,62 +1,45 @@
 {pkgs, ...}: {
   home = {
-    packages = with pkgs;
-      [
-        #~@ KDE Themes
-        plasma-overdose-kde-theme
-        materia-kde-theme
-        arc-kde-theme
-        twilight-kde
-        adapta-kde-theme
-        sweet-nova
-        utterly-nord-plasma
-        utterly-round-plasma-style
-        catppuccin-kde
+    packages = with pkgs; [
+      #~@ KDE Themes
+      plasma-overdose-kde-theme
+      materia-kde-theme
+      arc-kde-theme
+      twilight-kde
+      adapta-kde-theme
+      sweet-nova
+      utterly-nord-plasma
+      utterly-round-plasma-style
+      catppuccin-kde
 
-        #~@ SDDM Themes
-        catppuccin-sddm-corners
+      #~@ SDDM Themes
+      catppuccin-sddm-corners
 
-        #~@ Kvantum Themes
-        catppuccin-kvantum
-        gruvbox-kvantum
-        rose-pine-kvantum
-        ayu-theme-gtk
-        kvmarwaita
+      #~@ Kvantum Themes
+      catppuccin-kvantum
+      gruvbox-kvantum
+      rose-pine-kvantum
+      ayu-theme-gtk
+      kvmarwaita
 
-        #~@ GTK Themes
-        rose-pine-gtk-theme
-        jasper-gtk-theme
-        fluent-gtk-theme
-        colloid-gtk-theme
-        juno-theme
-        yaru-remix-theme
-        catppuccin-gtk
-        lavanda-gtk-theme
-        kanagawa-gtk-theme
-        magnetic-catppuccin-gtk
+      #~@ GTK Themes
+      rose-pine-gtk-theme
+      jasper-gtk-theme
+      fluent-gtk-theme
+      colloid-gtk-theme
+      juno-theme
+      yaru-remix-theme
+      catppuccin-gtk
+      lavanda-gtk-theme
+      kanagawa-gtk-theme
+      magnetic-catppuccin-gtk
 
-        #~@ Cursors
-        catppuccin-cursors
+      #~@ Cursors
+      catppuccin-cursors
 
-        #~@ Visuals
-        kurve
-        catppuccin
-      ]
-      ++ (with kdePackages; [
-        #~@ Themes
-        koi
-
-        #~@ Window Management
-        krohnkite
-
-        #~@ Applications
-        # plasmatube
-        # calindori
-        # karp
-
-        #~@ Utilities
-        kup
-        qxlsx
-      ]);
+      #~@ Visuals
+      kurve
+      catppuccin
+    ];
   };
 }
