ff53745f01d4dd9fca730c466990c05ceefb39bc
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-15 21:28:46 -0500

    Developing flake

--- Libraries/lists/enums/export.nix
+++ Libraries/lists/enums/export.nix
@@ -4,9 +4,15 @@
   # inherit (hardware) functionalities cpuBrands cpuPowerModes gpuBrands;
   # inherit (tui) shells;
   # inherit (user) roles capabilities;
+
+  dev = import ./development.nix {inherit _;};
+  gui = import ./gui.nix {inherit _;};
+  hardware = import ./hardware.nix {inherit _;};
+  tui = import ./tui.nix {inherit _;};
+  user = import ./user.nix {inherit _;};
   enums = {
-    developmentLanguages = imports.dev.languages;
-    inherit (imports) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
+    developmentLanguages = dev.languages;
+    inherit (gui) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
   };
 in
   enums // {_rootAliases = enums;}
