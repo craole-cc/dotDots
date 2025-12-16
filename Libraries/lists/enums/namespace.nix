bb6c3be81e68dfb5b409b8be3ffb09a73b5ac0bd
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-15 21:31:33 -0500

    Developing flake

--- Libraries/lists/enums/namespace.nix
+++ Libraries/lists/enums/namespace.nix
@@ -1,18 +1,35 @@
 # Libraries/lists/enums.nix
 {_, ...}: let
-  imports = _.lists.enums;
-  # inherit (hardware) functionalities cpuBrands cpuPowerModes gpuBrands;
-  # inherit (tui) shells;
-  # inherit (user) roles capabilities;
-
+  # Import all enum modules
   dev = import ./development.nix {inherit _;};
   gui = import ./gui.nix {inherit _;};
   hardware = import ./hardware.nix {inherit _;};
   tui = import ./tui.nix {inherit _;};
   user = import ./user.nix {inherit _;};
-  enums = {
-    developmentLanguages = dev.languages;
-    inherit (gui) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
+in {
+  # Export everything flat at root
+  inherit (dev) languages;
+  inherit (gui) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
+  inherit (hardware) functionalities cpuBrands cpuPowerModes gpuBrands;
+  inherit (tui) shells;
+  inherit (user) roles capabilities;
+
+  # Also export categorized
+  development = dev;
+  gui = gui;
+  hardware = hardware;
+  tui = tui;
+  user = user;
+
+  _rootAliases = {
+    # Flat access via root
+    enums = {
+      # All enums accessible here
+      developmentLanguages = dev.languages;
+      bootLoaders = gui.bootLoaders;
+      # ... etc
+      userRoles = user.roles;
+      userCapabilities = user.capabilities;
+    };
   };
-in
-  enums // {_rootAliases = enums;}
+}
