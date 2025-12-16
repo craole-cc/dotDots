da48ba1a6a28203289fa0cd1b6e718e2a679e5de
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-15 21:39:16 -0500

    Developing flake

--- Libraries/lists/enums/_.nix
+++ Libraries/lists/enums/_.nix
@@ -1,36 +1,37 @@
 # Libraries/lists/enums.nix
 {_, ...}: let
   # Import all enum modules
-  dev = _.lists.enums.development;
-  # dev = import ./development.nix {inherit _;};
-  gui = import ./gui.nix {inherit _;};
-  hardware = import ./hardware.nix {inherit _;};
-  tui = import ./tui.nix {inherit _;};
-  user = import ./user.nix {inherit _;};
+  e = _.lists.enums;
+  # # dev = import ./development.nix {inherit _;};
+  # gui = import ./gui.nix {inherit _;};
+  # hardware = import ./hardware.nix {inherit _;};
+  # tui = import ./tui.nix {inherit _;};
+  # user = import ./user.nix {inherit _;};
+  developmentLanguages = e.dev.languages;
 in {
   # Export everything flat at root
-  inherit (dev) languages;
-  inherit (gui) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
-  inherit (hardware) functionalities cpuBrands cpuPowerModes gpuBrands;
-  inherit (tui) shells;
-  inherit (user) roles capabilities;
+  inherit developmentLanguages;
+  inherit (e.gui) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
+  inherit (e.hardware) functionalities cpuBrands cpuPowerModes gpuBrands;
+  inherit (e.tui) shells;
+  inherit (e.user) roles capabilities;
 
   # Also export categorized
-  development = dev;
-  gui = gui;
-  hardware = hardware;
-  tui = tui;
-  user = user;
+  # development = dev;
+  # gui = gui;
+  # hardware = hardware;
+  # tui = tui;
+  # user = user;
 
   _rootAliases = {
     # Flat access via root
     enums = {
       # All enums accessible here
-      developmentLanguages = dev.languages;
-      bootLoaders = gui.bootLoaders;
+      inherit developmentLanguages;
+      bootLoaders = e.gui.bootLoaders;
       # ... etc
-      userRoles = user.roles;
-      userCapabilities = user.capabilities;
+      userRoles = e.user.roles;
+      userCapabilities = e.user.capabilities;
     };
   };
 }
