c09e896ad910b89da1c8e3fa458165d7fd9bdc6d
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-16 10:46:27 -0500

    Developing flake

--- API/users/craole/programs/foot/themes.nix
+++ API/users/craole/programs/foot/themes.nix
@@ -1,12 +1,10 @@
 {
-  settings = {
-    colors = {
-      alpha = 0.95; # Slight transparency (1.0 = opaque)
-    };
+  colors = {
+    alpha = 0.95; # Slight transparency (1.0 = opaque)
+  };
 
-    cursor = {
-      style = "beam"; # Options: block, underline, beam
-      blink = "yes";
-    };
+  cursor = {
+    style = "beam"; # Options: block, underline, beam
+    blink = "yes";
   };
 }
