1e69b52e30755a621297af3269e708a6e368c710
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-16 18:43:56 -0500

    Developing flake

--- Libraries/types/_.nix
+++ Libraries/types/_.nix
@@ -1,27 +1,7 @@
 {_, ...}: let
-  exports = _.types.predicates;
+  types = _.types.predicates;
 in
-  exports
-# // {
-#   _rootAliases = {inherit enums;};
-#   _tests = runTests {
-#     #? Test structure exists
-#     hasValues = mkTest true (hasAttrByPath ["enums" "developmentLanguages" "values"] _);
-#     hasValidator = mkTest true (hasAttrByPath ["enums" "developmentLanguages" "validator"] _);
-#     hasRootAlias = mkTest true (hasAttrByPath ["enums" "developmentLanguages"] _);
-#     #? Test actual functionality
-#     validatorWorks = mkTest true (_.enums.developmentLanguages.validator.check "rust");
-#     valuesIsList = mkTest true (isList (_.enums.developmentLanguages.values or null));
-#     #? Test multiple enums exist
-#     hasAllCategories = mkTest true (
-#       true
-#       && hasAttrByPath ["enums" "developmentLanguages"] _
-#       && hasAttrByPath ["enums" "hostFunctionalities"] _
-#       && hasAttrByPath ["enums" "userRoles"] _
-#       && hasAttrByPath ["enums" "bootLoaders"] _
-#       && hasAttrByPath ["enums" "shells"] _
-#     );
-#     # Test enum count
-#     hasExpectedEnumCount = mkTest 14 (length (attrNames enums));
-#   };
-# }
+  types
+  // {
+    _rootAliases = {inherit types;};
+  }
