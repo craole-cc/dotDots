01a89663b52161e8a76ed6da3a47043adf620e7f
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-15 21:53:40 -0500

    Developing flake

--- Libraries/lists/enums/_.nix
+++ Libraries/lists/enums/_.nix
@@ -1,13 +1,24 @@
 {_, ...}: let
   e = _.lists.enums;
+
+  /**
+      Flat enum aggregator - provides both direct and namespaced access.
+
+      # Access Patterns
+  ```nix
+      _.lists.desktopEnvironments           # Direct (flat)
+      _.lists._.desktopEnvironments         # Namespaced
+      _.enums.desktopEnvironments           # Root alias
+  ```
+  */
   enums = {
-    developmentLanguages = e.development.languages;
-    userRoles = e.user.roles;
-    userCapabilities = e.user.capabilities;
-    hostCapabilities = e.hardware.functionalities;
     inherit (e.gui) bootLoaders displayProtocols displayManagers desktopEnvironments windowManagers waylandSupport;
     inherit (e.hardware) cpuBrands cpuPowerModes gpuBrands;
     inherit (e.tui) shells;
+    developmentLanguages = e.development.languages;
+    userRoles = e.user.roles;
+    userCapabilities = e.user.capabilities;
+    hostFunctionalities = e.hardware.functionalities;
   };
 in
   enums // {_rootAliases = {inherit enums;};}
