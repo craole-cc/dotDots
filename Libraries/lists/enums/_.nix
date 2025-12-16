70241a86a17cd4357de724b86bd4b12f009d170a
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-15 22:24:32 -0500

    Developing flake

--- Libraries/lists/enums/_.nix
+++ Libraries/lists/enums/_.nix
@@ -4,8 +4,9 @@
   ...
 }: let
   e = _.lists.enums;
-  inherit (lib.attrsets) hasAttr hasAttrByPath;
   inherit (_.testing.unit) mkTest runTests;
+  inherit (lib.attrsets) attrNames hasAttrByPath;
+  inherit (lib.lists) isList length;
 
   /**
       Enum aggregator - provides flat access to all enums.
@@ -52,8 +53,25 @@ in
   // {
     _rootAliases = {inherit enums;};
     _tests = runTests {
-      values = mkTest true (hasAttrByPath ["enums" "developmentLanguages" "values"] _);
-      validators = mkTest true (hasAttrByPath ["enums" "developmentLanguages" "validator"] _);
-      rootAlias = mkTest true (hasAttrByPath ["enums" "developmentLanguages"] _);
+      #? Test structure exists
+      hasValues = mkTest true (hasAttrByPath ["enums" "developmentLanguages" "values"] _);
+      hasValidator = mkTest true (hasAttrByPath ["enums" "developmentLanguages" "validator"] _);
+      hasRootAlias = mkTest true (hasAttrByPath ["enums" "developmentLanguages"] _);
+
+      #? Test actual functionality
+      validatorWorks = mkTest true (_.enums.developmentLanguages.validator.check "rust");
+      valuesIsList = mkTest true (isList (_.enums.developmentLanguages.values or null));
+
+      #? Test multiple enums exist
+      hasAllCategories = mkTest true (
+        hasAttrByPath ["enums" "developmentLanguages"] _
+        && hasAttrByPath ["enums" "hostFunctionalities"] _
+        && hasAttrByPath ["enums" "userRoles"] _
+        && hasAttrByPath ["enums" "bootLoaders"] _
+        && hasAttrByPath ["enums" "shells"] _
+      );
+
+      # Test enum count
+      hasExpectedEnumCount = mkTest 14 (length (attrNames enums));
     };
   }
