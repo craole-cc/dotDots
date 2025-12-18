4b0900e3e8da7f66d9e039bfec4e5672fcabe5b9
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-18 02:38:38 -0500

    Developing flake

--- API/users/craole/programs/common/default.nix
+++ API/users/craole/programs/common/default.nix
@@ -1,10 +1,20 @@
 {lix, ...}: {
-  # imports = [
-  #   ./delta.nix
-  #   ./git.nix
-  #   ./github.nix
-  #   ./gitui.nix
-  #   ./jujutsu.nix
-  # ];
-  imports = lix.importAll ./.;
+  imports = [
+    ./bat
+    ./default
+    ./delta
+    ./direnv
+    ./fastfetch
+    ./git
+    ./github
+    ./gitui
+    ./grep
+    ./jujutsu
+    ./nh
+    ./nix-index
+    ./script
+    ./topgrade
+    ./yazi
+  ];
+  # imports = lix.importAll ./.;
 }
