834ca4a44e31f600b9b71822b253d061c9e12b24
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-18 03:07:31 -0500

    Developing flake

--- shell.nix
+++ shell.nix
@@ -1,33 +1,37 @@
-{pkgs ? import <nixpkgs> {}}: {
-  default = pkgs.mkShell {
-    packages = with pkgs; [
+# shell.nix - Use the flake's nixpkgs input
+{pkgs ? null}: let
+  # If pkgs is not passed, try to get it from the flake
+  finalPkgs =
+    if pkgs != null
+    then pkgs
+    else
+      (
+        if builtins ? getFlake
+        then (builtins.getFlake (toString ./.)).inputs.nixosCore.legacyPackages.${builtins.currentSystem}
+        else import <nixpkgs> {}
+      );
+in {
+  default = finalPkgs.mkShell {
+    packages = with finalPkgs; [
       nix
       helix
       alejandra
       git
     ];
 
-    env = {
-      NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-    };
-
     shellHook = ''
       printf "🚀 Project shell loaded\n"
 
-      # Add current project's bin directory to PATH
-      # This is where your nix-repl script should be symlinked
       PROJECT_BIN="$PWD/.bin"
       mkdir -p "$PROJECT_BIN"
 
-      # Create symlink to the actual script
       if [ -f "$PWD/Bin/shellscript/project/nix/nix-repl" ]; then
         ln -sf "$PWD/Bin/shellscript/project/nix/nix-repl" "$PROJECT_BIN/repl"
         ln -sf "$PWD/Bin/shellscript/project/nix/nix-repl" "$PROJECT_BIN/nix-repl"
       fi
 
       export PATH="$PROJECT_BIN:$PATH"
-
-      printf "Run 'repl' or 'nix-repl' to start Nix REPL\n"
+      printf "Run 'repl' to start Nix REPL\n"
     '';
   };
 }
