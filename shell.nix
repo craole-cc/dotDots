a521c3214264ca13a35ef66338728db46c972c43
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-18 01:47:07 -0500

    adding devShells

--- shell.nix
+++ shell.nix
@@ -1,17 +1,40 @@
-# {pkgs ? import <nixpkgs> {}}: {
-#   default = pkgs.mkShell {
-#     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-#     shellHook = "nix develop";
-#   };
-# }
 {pkgs ? import <nixpkgs> {}}: {
   default = pkgs.mkShell {
-    shellHook = ''
-      # Set NIX_CONFIG only when needed
-      export NIX_CONFIG="extra-experimental-features = nix-command flakes"
+    packages = with pkgs; [
+      nix
+      helix
+      alejandra
+      git
+    ];
 
-      # Debug: Check environment size
-      echo "Environment size: $(env | wc -c) bytes"
+    env = rec {
+      NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+      NIX_FLAKE = toString ./.;
+      NIX_REPL = "${NIX_FLAKE}/Bin/shellscript/project/nix/nix-repl";
+    };
+
+    shellHook = ''
+      printf "🚀 Project shell loaded\n"
+      printf "FLAKE: %s\n" "$NIX_FLAKE"
+      printf "REPL: %s\n" "$NIX_REPL"
+
+      # Define shell functions (not aliases)
+      repl() {
+        if [ -f "$NIX_REPL" ]; then
+          "$NIX_REPL" "$@"
+        else
+          printf "Error: REPL not found at %s\n" "$NIX_REPL" >&2
+          return 1
+        fi
+      }
+
+      nix-repl() {
+        repl "$@"
+      }
+
+      printf "Available commands:\n"
+      printf "  repl       - Run Nix REPL\n"
+      printf "  nix-repl   - Same as 'repl'\n"
     '';
   };
 }
