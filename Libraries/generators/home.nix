193391067f5a0b28b25abbc618c4e6d59c2a07c7
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-17 23:46:33 -0500

    Developing flake

--- Libraries/generators/home.nix
+++ Libraries/generators/home.nix
@@ -308,7 +308,10 @@
 
           #> Enable shells in home-manager
           programs = {
-            bash.enable = mkDefault (elem "bash" (cfg.shells or []));
+            bash = {
+              enable = mkDefault (elem "bash" (cfg.shells or []));
+              blesh.enable = true;
+            };
             zsh.enable = mkDefault (elem "zsh" (cfg.shells or []));
             fish.enable = mkDefault (elem "fish" (cfg.shells or []));
             nushell.enable = mkDefault (elem "nushell" (cfg.shells or []));
