d69917d9f31a3c80bdfd481dd67ab8f18d7922c3
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-18 19:40:18 -0500

    Developing flake

--- default.nix
+++ default.nix
@@ -47,7 +47,7 @@
     inherit (lib.lists) unique;
     inherit (lib.attrsets) genAttrs mapAttrsToList;
     current = builtins.currentSystem;
-    defined = mapAttrsToList (_: host: host.systemd or host.platform or {}) hosts;
+    defined = mapAttrsToList (_: host: host.systemd or host.platforms or {}) hosts;
     popular =
       if nix-systems != null
       then import nix-systems
