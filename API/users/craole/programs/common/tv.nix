ede1ec3e51524cd75c1dac14c3da8223af911a20
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-20 02:14:37 -0500

    adding nix search tv

--- API/users/craole/programs/common/tv.nix
+++ API/users/craole/programs/common/tv.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    (writeShellApplication {
-      name = "ns";
-      runtimeInputs = [
-        fzf
-        nix-search-tv
-      ];
-      text = builtins.readFile "${nix-search-tv.src}/nixpkgs.sh";
-    })
+    nix-search-tv
+    # (writeShellApplication {
+    #   name = "ns";
+    #   runtimeInputs = [
+    #     fzf
+    #     nix-search-tv
+    #   ];
+    #   text = builtins.readFile "${nix-search-tv.src}/nixpkgs.sh";
+    # })
   ];
 }
