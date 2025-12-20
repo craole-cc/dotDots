3f4516b10b7f9853024817e8fc115399840dbc32
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-20 15:55:11 -0500

    Developing flake

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
 {
-  lib,
-  src,
+  lib ? import <nixpkgs/lib>,
+  src ? ./.,
   ...
 }: let
   inherit (import ./Libraries {inherit lib src;}) lix;
   inherit (import ./API {inherit lix;}) hosts users;
-in {inherit lix users hosts;}
+
+  repl = import ./Packages/cli/repl {inherit lib src lix hosts users;};
+in {
+  inherit lix users hosts repl;
+}
