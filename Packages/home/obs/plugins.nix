f5c6258165e8643df87593ce46f3685eaa73ccac
Craole <32288735+Craole@users.noreply.github.com>
2025-12-29 03:13:59 -0500

    activating the packages one by one, now at obs plugins

--- Packages/home/obs/plugins.nix
+++ Packages/home/obs/plugins.nix
@@ -3,6 +3,7 @@
   lib,
   lix,
   user,
+  config,
   ...
 }: let
   inherit (lib.lists) optionals;
@@ -12,30 +13,30 @@ in
     plugins =
       [
         droidcam-obs
-        # looking-glass-obs
+        looking-glass-obs
         obs-advanced-masks
         obs-aitum-multistream
         obs-backgroundremoval
-        # obs-color-monitor
-        # obs-composite-blur
-        # obs-dir-watch-media
-        # obs-browser-transition
-        # obs-freeze-filter
-        # obs-livesplit-one
-        # obs-media-controls
-        # obs-move-transition
-        # obs-multi-rtmp
-        # obs-mute-filter
-        # obs-replay-source
-        # obs-retro-effects
-        # obs-rgb-levels
-        # obs-scale-to-sound
-        # obs-source-record
-        # obs-source-switcher
-        # obs-transition-table
-        # obs-tuna
-        # obs-vertical-canvas
-        # obs-vintage-filter
+        obs-color-monitor
+        obs-composite-blur
+        obs-dir-watch-media
+        obs-browser-transition
+        obs-freeze-filter
+        obs-livesplit-one
+        obs-media-controls
+        obs-move-transition
+        obs-multi-rtmp
+        obs-mute-filter
+        obs-replay-source
+        obs-retro-effects
+        obs-rgb-levels
+        obs-scale-to-sound
+        obs-source-record
+        obs-source-switcher
+        obs-transition-table
+        obs-tuna
+        obs-vertical-canvas
+        obs-vintage-filter
         waveform
       ]
       ++ optionals (
