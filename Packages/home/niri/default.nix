dcb42276fe4c637650e352dadcdc62c507f3025f
Craole <32288735+Craole@users.noreply.github.com>
2025-12-29 01:19:46 -0500

    niri switcher

--- Packages/home/niri/default.nix
+++ Packages/home/niri/default.nix
@@ -1,10 +1,22 @@
-{user, ...}: let
+{
+  user,
+  lib,
+  ...
+}: let
   app = "niri";
   isAllowed = (user.interface.windowManager or null) == app;
+  inherit (lib.modules) mkIf;
 in {
-  programs.${app} =
-    {enable = true;}
-    // import ./bindings.nix
-    // import ./layout.nix
-    // import ./settings.nix;
+  programs = mkIf isAllowed {
+    # ${app} =
+    #   {enable = true;}
+    #   // import ./bindings.nix
+    #   // import ./layout.nix
+    #   // import ./settings.nix;
+
+    niriswitcher =
+      {enable = true;}
+      // import ./switcher/settings.nix
+      // import ./switcher/style.nix;
+  };
 }
