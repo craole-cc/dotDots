3eafaa7921be31fadc8e619ccfbb3bd0ab0cb827
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-29 20:06:18 -0500

    plasma

--- Packages/home/interface/plasma/default.nix
+++ Packages/home/interface/plasma/default.nix
@@ -19,13 +19,13 @@ in {
         {enable = true;}
         // import ./bindings
         // import ./files
-        // import ./modules/input.nix
-        // import ./modules/launcher.nix
-        // import ./modules/power.nix
-        // import ./modules/screenlock.nix
-        // import ./modules/screenshot.nix
-        // import ./modules/session.nix
-        // import ./modules/windows.nix
+        # // import ./modules/input.nix
+        # // import ./modules/launcher.nix
+        # // import ./modules/power.nix
+        # // import ./modules/screenlock.nix
+        # // import ./modules/screenshot.nix
+        # // import ./modules/session.nix
+        # // import ./modules/windows.nix
         // import ./modules/workspace.nix {inherit pkgs;};
     };
 
