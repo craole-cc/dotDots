27756fac0cefd7955da4c4037d2c9eb1840c7dbe
craole-cc <134658831+craole-cc@users.noreply.github.com>
2025-12-29 21:54:08 -0500

    plasma

--- Packages/home/interface/plasma/bindings/applications.nix
+++ Packages/home/interface/plasma/bindings/applications.nix
@@ -1,5 +1,5 @@
 {
   "services/com.mitchellh.ghostty.desktop"._launch = ["Meta+Return" "Ctrl+Alt+T"];
   "services/org.kde.konsole.desktop"._launch = ["Meta+Alr+K"];
-  "yakuake.toggle-window-state" = ["Meta+`" "Shift+F12"];
+  yakuake.toggle-window-state = ["Meta+`" "Shift+F12"];
 }
