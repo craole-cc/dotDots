3e1b39c6d52347fc277e343b9a452ba369a1f408
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 18:47:31 -0500

    feetPackage

--- Packages/home/terminal/foot/default.nix
+++ Packages/home/terminal/foot/default.nix
@@ -20,21 +20,46 @@
     # debug = true;
   };
 
+  # bin = rec {
+  #   foot = getExe' app.package "foot";
+  #   footclient = getExe' app.package "footclient";
+  #   feet = pkgs.writeShellScriptBin app.command ''
+  #     if ${footclient} --no-wait 2>/dev/null; then exit 0 ; else
+  #       ${foot} --server &
+  #       sleep 0.1
+  #       exec ${footclient}
+  #     fi
+  #   '';
+  # };
+
   bin = rec {
     foot = getExe' app.package "foot";
     footclient = getExe' app.package "footclient";
-    feet = pkgs.writeShellScriptBin app.command ''
-      if ${footclient} --no-wait 2>/dev/null; then exit 0 ; else
+
+    feet = pkgs.writeShellScriptBin "foot" ''
+      if ! ${footclient} --no-wait 2>/dev/null; then
         ${foot} --server &
         sleep 0.1
-        exec ${footclient}
       fi
+      exec ${footclient} "$@"
     '';
+
+    # Create a complete package with desktop files and icons
+    feetPackage = pkgs.symlinkJoin {
+      name = "feet";
+      paths = [feet app.package];
+      postBuild = ''
+        # Override the foot binary with our wrapper
+        rm -f $out/bin/foot
+        ln -s ${feet}/bin/foot $out/bin/foot
+        ln -s $out/bin/foot $out/bin/feet
+      '';
+    };
   };
 
   cfg = userApplicationConfig {
     inherit app user pkgs config;
-    extraPackages = [bin.feet];
+    extraPackages = [bin.feetPackage];
     extraProgramConfig = {
       server.enable = true;
       settings =
