3c5d3a8bf47425bfa4db9241a22dd8e57d1be0d8
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 12:21:31 -0500

    plasma screenlocker wallpaper

--- Packages/home/interface/plasma/modules/screenlock.nix
+++ Packages/home/interface/plasma/modules/screenlock.nix
@@ -1,10 +1,11 @@
-{
+{src}: {
   kscreenlocker = {
     lockOnResume = true;
     timeout = 10;
     appearance = {
       alwaysShowClock = true;
       showMediaControls = false;
+      wallpaper = src + "/Assets/Images/wallpaper/16x9/unsorted/wallhaven-1jwwdv.jpg";
     };
   };
 }
