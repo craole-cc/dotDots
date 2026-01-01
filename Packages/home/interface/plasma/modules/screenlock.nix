2772ea18017e97f380ce500fe91ba1a746be19ac
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 12:48:47 -0500

    sync 2026-01-01 12:48

--- Packages/home/interface/plasma/modules/screenlock.nix
+++ Packages/home/interface/plasma/modules/screenlock.nix
@@ -5,7 +5,13 @@
     appearance = {
       alwaysShowClock = true;
       showMediaControls = false;
-      wallpaper = src + "/Assets/Images/wallhaven-4y5glk.jpg";
+      # wallpaper = src + "/Assets/Images/wallpapers/wallpaper_lockscreen_dark.jpg";
+      wallpaperPictureOfTheDay = {
+        provider = "bing";
+      };
+      # wallpaperSlideShow = {
+      #   path = src + "/Assets/Images/wallpapers";
+      # };
     };
   };
 }
