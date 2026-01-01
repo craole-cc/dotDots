3cbc3d771836f4a61a7df340c66ad8a2769788a9
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 15:43:31 -0500

    cursor theme

--- Packages/home/interface/plasma/modules/workspace.nix
+++ Packages/home/interface/plasma/modules/workspace.nix
@@ -6,15 +6,16 @@
   wallpapers = config.home.homeDirectory + "/Pictures/Wallpapers";
   # wallpapers = "/home/craole/.dots/Assets/Images/wallpaper";
   # wallpapers = "/home/craole/Pictures/Wallpapers";
+  # pkgs.candy-icons
 in {
   workspace = {
     clickItemTo = "open"; # If you liked the click-to-open default from plasma 5
     lookAndFeel = "org.kde.breezedark.desktop";
     cursor = {
-      theme = "Bibata-Modern-Ice";
+      theme = "material_cursors";
       size = 32;
     };
-    iconTheme = "Papirus-Dark";
+    iconTheme = "candy-icons";
     # wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
     wallpaperBackground = {
       blur = true;
