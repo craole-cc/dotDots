334b3562de0c97da4ade66fcf0a77a7d35258ef5
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 15:12:24 -0500

    desktop wallpaper

--- Packages/home/interface/plasma/modules/workspace.nix
+++ Packages/home/interface/plasma/modules/workspace.nix
@@ -1,8 +1,12 @@
 {
-  pkgs,
+  # pkgs,
   config,
   ...
-}: {
+}: let
+  wallpapers = config.home.homeDirectory + "Pictures/Wallpapers";
+  # wallpapers = "/home/craole/.dots/Assets/Images/wallpaper";
+  # wallpapers = "/home/craole/Pictures/Wallpapers";
+in {
   workspace = {
     clickItemTo = "open"; # If you liked the click-to-open default from plasma 5
     lookAndFeel = "org.kde.breezedark.desktop";
@@ -12,6 +16,8 @@
     };
     iconTheme = "Papirus-Dark";
     # wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
-    wallpaper = config.home.homeDirectory + "Pictures/Wallpapers";
+    wallpaperSlideShow = {
+      path = wallpapers;
+    };
   };
 }
