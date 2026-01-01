bfe17fb8afd4504cdc74c33142079ef28d9a9e68
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 15:54:56 -0500

    theme packages

--- Packages/home/interface/plasma/default.nix
+++ Packages/home/interface/plasma/default.nix
@@ -25,14 +25,63 @@ in {
     };
 
     home = {
-      packages = with pkgs.kdePackages; [
-        koi
-        krohnkite
-        yakuake
-      ];
       shellAliases = {
         plasma-config-dump = "nix run github:nix-community/plasma-manager > $DOTS/Packages/home/interface/plasma/dump.nix";
       };
+      packages = with pkgs;
+        [
+          #~@ KDE Themes
+          plasma-overdose-kde-theme
+          materia-kde-theme
+          arc-kde-theme
+          twilight-kde
+          adapta-kde-theme
+          sweet-nova
+          utterly-nord-plasma
+          utterly-round-plasma-style
+          catppuccin-kde
+
+          #~@ SDDM Themes
+          catppuccin-sddm-corners
+
+          #~@ Kvantum Themes
+          catppuccin-kvantum
+          gruvbox-kvantum
+          rose-pine-kvantum
+          ayu-theme-gtk
+          kvmarwaita
+          rose-pine-kvantum
+
+          #~@ GTK Themes
+          rose-pine-gtk-theme
+          jasper-gtk-theme
+          fluent-gtk-theme
+          colloid-gtk-theme
+          juno-theme
+          yaru-remix-theme
+          catppuccin-gtk
+          lavanda-gtk-theme
+          kanagawa-gtk-theme
+          magnetic-catppuccin-gtk
+          rose-pine-gtk-theme
+
+          #~@ Cursors
+          catppuccin-cursors
+          rose-pine-cursor
+
+          #~@ Icons
+          candy-icons
+          rose-pine-icon-theme
+
+          #~@ Visuals
+          kurve
+          catppuccin
+        ]
+        ++ (with kdePackages; [
+          koi
+          krohnkite
+          yakuake
+        ]);
     };
   };
 }
