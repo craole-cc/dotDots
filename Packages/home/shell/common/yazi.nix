5b5b60caa8e6e34a63d0de2c60d32398b1769d73
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-02 00:18:13 -0500

    yazi

--- Packages/home/shell/common/yazi.nix
+++ Packages/home/shell/common/yazi.nix
@@ -4,45 +4,47 @@
   };
 
   home = {
-    packages = with pkgs.yaziPlugins; [
-      bookmarks
-      bypass
-      chmod
-      compress
-      diff
-      duckdb
-      dupes
-      full-border
-      gitui
-      glow
-      lazygit
-      lsar
-      mactag
-      mediainfo
-      miller
-      mime-ext
-      mount
-      no-status
-      nord
-      ouch
-      piper
-      projects
-      recycle-bin
-      relative-motions
-      restore
-      rich-preview
-      rsync
-      smart-enter
-      smart-filter
-      smart-paste
-      starship
-      sudo
-      time-travel
-      toggle-pane
-      vcs-files
-      wl-clipboard
-      yatline-catppuccin
-      yatline-githead
-    ];
+    packages = with pkgs.yaziPlugins;
+      [
+        bookmarks
+        bypass
+        chmod
+        compress
+        diff
+        duckdb
+        dupes
+        full-border
+        gitui
+        glow
+        lazygit
+        lazygit
+        lsar
+        mediainfo
+        miller
+        mime-ext
+        mount
+        no-status
+        nord
+        ouch
+        piper
+        projects
+        recycle-bin
+        relative-motions
+        restore
+        rich-preview
+        rsync
+        smart-enter
+        smart-filter
+        smart-paste
+        starship
+        sudo
+        time-travel
+        toggle-pane
+        vcs-files
+        wl-clipboard
+        yatline-catppuccin
+        yatline-githead
+      ]
+      ++ optionals (pkgs.stdenv.isDarwin) [mactag];
   };
 }
