cfde14806f43959ed66c3bc231f660eea8ae1762
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 21:05:42 -0500

    konsoole shortcut

--- Packages/home/interface/plasma/bindings/applications.nix
+++ Packages/home/interface/plasma/bindings/applications.nix
@@ -1,7 +1,7 @@
 {
   "services/feet.desktop"._launch = ["Meta+Return" "Meta+Shift+F"];
   "services/net.local.feet-quake.desktop"._launch = ["Meta+~"];
-  "services/com.mitchellh.ghostty.desktop"._launch = ["Ctrl+Alt+T"];
-  "services/org.kde.konsole.desktop"._launch = ["Meta+Alr+K"];
+  "services/com.mitchellh.ghostty.desktop"._launch = ["Meta+Shift+G"];
+  "services/org.kde.konsole.desktop"._launch = ["Meta+Shift+K"];
   yakuake.toggle-window-state = ["Shift+F12" "Meta+`"];
 }
