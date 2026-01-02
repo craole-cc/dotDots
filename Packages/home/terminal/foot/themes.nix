511fe7f61d5c227f9bb940911091f6e900cabb49
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-01 19:54:52 -0500

    foot theme

--- Packages/home/terminal/foot/themes.nix
+++ Packages/home/terminal/foot/themes.nix
@@ -1,64 +1,34 @@
 {
-  #~@ Catppuccin Frappe (Dark Theme)
-  #? Active when system is in dark mode
-  colors-dark = {
+  #~@ Catppuccin Frappe (Dark)
+  colors = {
     alpha = 0.97;
     background = "303446";
     foreground = "c6d0f5";
 
     #~@ Regular Colors (0-7)
-    regular0 = "51576d"; # black
-    regular1 = "e78284"; # red
-    regular2 = "a6d189"; # green
-    regular3 = "e5c890"; # yellow
-    regular4 = "8caaee"; # blue
-    regular5 = "f4b8e4"; # magenta
-    regular6 = "81c8be"; # cyan
-    regular7 = "b5bfe2"; # white
+    regular0 = "51576d";
+    regular1 = "e78284";
+    regular2 = "a6d189";
+    regular3 = "e5c890";
+    regular4 = "8caaee";
+    regular5 = "f4b8e4";
+    regular6 = "81c8be";
+    regular7 = "b5bfe2";
 
     #~@ Bright Colors (8-15)
-    bright0 = "626880"; # bright black
-    bright1 = "e78284"; # bright red
-    bright2 = "a6d189"; # bright green
-    bright3 = "e5c890"; # bright yellow
-    bright4 = "8caaee"; # bright blue
-    bright5 = "f4b8e4"; # bright magenta
-    bright6 = "81c8be"; # bright cyan
-    bright7 = "a5adce"; # bright white
-  };
-
-  #~@ Catppuccin Latte (Light Theme)
-  #? Active when system is in light mode
-  colors-light = {
-    alpha = 0.97;
-    background = "eff1f5";
-    foreground = "4c4f69";
-
-    #~@ Regular Colors (0-7)
-    regular0 = "5c5f77"; # black
-    regular1 = "d20f39"; # red
-    regular2 = "40a02b"; # green
-    regular3 = "df8e1d"; # yellow
-    regular4 = "1e66f5"; # blue
-    regular5 = "ea76cb"; # magenta
-    regular6 = "179299"; # cyan
-    regular7 = "acb0be"; # white
-
-    #~@ Bright Colors (8-15)
-    bright0 = "6c6f85"; # bright black
-    bright1 = "d20f39"; # bright red
-    bright2 = "40a02b"; # bright green
-    bright3 = "df8e1d"; # bright yellow
-    bright4 = "1e66f5"; # bright blue
-    bright5 = "ea76cb"; # bright magenta
-    bright6 = "179299"; # bright cyan
-    bright7 = "bcc0cc"; # bright white
+    bright0 = "626880";
+    bright1 = "e78284";
+    bright2 = "a6d189";
+    bright3 = "e5c890";
+    bright4 = "8caaee";
+    bright5 = "f4b8e4";
+    bright6 = "81c8be";
+    bright7 = "a5adce";
   };
 
   #~@ Cursor Configuration
-  #? Visual appearance and behavior of the text cursor
   cursor = {
-    style = "beam"; #? Options: block, underline, beam
+    style = "beam";
     blink = "yes";
     blink-rate = 500;
     beam-thickness = 1.5;
