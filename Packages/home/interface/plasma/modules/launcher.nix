75cec3742dad8d04391f69a8cd1d7bdfda1654ab
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-02 02:36:59 -0500

    toggle terminal

--- Packages/home/interface/plasma/modules/launcher.nix
+++ Packages/home/interface/plasma/modules/launcher.nix
@@ -1,4 +1,9 @@
 {
+  hotkeys.commands."launch-foot-quake" = {
+    name = "Launch Foot Quake";
+    key = "Meta+~";
+    command = "feet --quake";
+  };
   krunner = {};
   spectacle = {};
 }
--- Packages/home/interface/plasma/packages.nix
+++ Packages/home/interface/plasma/packages.nix
@@ -1,73 +1,56 @@
-{pkgs, ...}: let
-  toggleTerminal = pkgs.stdenv.mkDerivation rec {
-    pname = "kwin-toggleterminal";
-    version = "unstable-2024";
-    src = pkgs.fetchFromGitHub {
-      owner = "DvdGiessen";
-      repo = pname;
-      rev = "main";
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"; # Run: nix-prefetch-github --owner DvdGiessen --repo kwin-toggleterminal --rev main
-    };
-    installPhase = ''
-      mkdir -p $out/share/kwin/scripts/${pname}
-      cp -r . $out/share/kwin/scripts/${pname}
-    '';
-  };
-in
-  with pkgs;
-    [
-      toggleTerminal
+{pkgs, ...}:
+with pkgs;
+  [
+    #~@ KDE Themes
+    plasma-overdose-kde-theme
+    materia-kde-theme
+    arc-kde-theme
+    twilight-kde
+    adapta-kde-theme
+    sweet-nova
+    utterly-nord-plasma
+    utterly-round-plasma-style
+    catppuccin-kde
 
-      #~@ KDE Themes
-      plasma-overdose-kde-theme
-      materia-kde-theme
-      arc-kde-theme
-      twilight-kde
-      adapta-kde-theme
-      sweet-nova
-      utterly-nord-plasma
-      utterly-round-plasma-style
-      catppuccin-kde
+    #~@ SDDM Themes
+    catppuccin-sddm-corners
 
-      #~@ SDDM Themes
-      catppuccin-sddm-corners
+    #~@ Kvantum Themes
+    catppuccin-kvantum
+    gruvbox-kvantum
+    rose-pine-kvantum
+    ayu-theme-gtk
+    kvmarwaita
+    rose-pine-kvantum
 
-      #~@ Kvantum Themes
-      catppuccin-kvantum
-      gruvbox-kvantum
-      rose-pine-kvantum
-      ayu-theme-gtk
-      kvmarwaita
-      rose-pine-kvantum
+    #~@ GTK Themes
+    rose-pine-gtk-theme
+    jasper-gtk-theme
+    fluent-gtk-theme
+    colloid-gtk-theme
+    juno-theme
+    yaru-remix-theme
+    catppuccin-gtk
+    lavanda-gtk-theme
+    kanagawa-gtk-theme
+    magnetic-catppuccin-gtk
+    rose-pine-gtk-theme
 
-      #~@ GTK Themes
-      rose-pine-gtk-theme
-      jasper-gtk-theme
-      fluent-gtk-theme
-      colloid-gtk-theme
-      juno-theme
-      yaru-remix-theme
-      catppuccin-gtk
-      lavanda-gtk-theme
-      kanagawa-gtk-theme
-      magnetic-catppuccin-gtk
-      rose-pine-gtk-theme
+    #~@ Cursors
+    catppuccin-cursors
+    rose-pine-cursor
 
-      #~@ Cursors
-      catppuccin-cursors
-      rose-pine-cursor
+    #~@ Icons
+    candy-icons
+    rose-pine-icon-theme
 
-      #~@ Icons
-      candy-icons
-      rose-pine-icon-theme
-
-      #~@ Visuals
-      kurve
-      catppuccin
-    ]
-    ++ (with kdePackages; [
-      koi
-      kpackage
-      krohnkite
-      yakuake
-    ])
+    #~@ Visuals
+    kurve
+    catppuccin
+  ]
+  ++ (with kdePackages; [
+    koi
+    kpackage
+    krohnkite
+    yakuake
+  ])
