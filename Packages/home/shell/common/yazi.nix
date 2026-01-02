5b8b931885d56c619eaf3185b0908a30cce583f1
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-02 00:29:34 -0500

    yazi

--- Packages/home/shell/common/yazi.nix
+++ Packages/home/shell/common/yazi.nix
@@ -18,38 +18,38 @@ in {
         # compress
         # diff
         duckdb
-        dupes
-        full-border
-        gitui
-        glow
-        lazygit
-        lazygit
-        lsar
-        mediainfo
-        miller
-        mime-ext
-        mount
-        no-status
-        nord
-        ouch
-        piper
-        projects
-        recycle-bin
-        relative-motions
-        restore
-        rich-preview
-        rsync
-        smart-enter
-        smart-filter
-        smart-paste
-        starship
-        sudo
-        time-travel
-        toggle-pane
-        vcs-files
-        wl-clipboard
-        yatline-catppuccin
-        yatline-githead
+        # dupes
+        # full-border
+        # gitui
+        # glow
+        # lazygit
+        # lazygit
+        # lsar
+        # mediainfo
+        # miller
+        # mime-ext
+        # mount
+        # no-status
+        # nord
+        # ouch
+        # piper
+        # projects
+        # recycle-bin
+        # relative-motions
+        # restore
+        # rich-preview
+        # rsync
+        # smart-enter
+        # smart-filter
+        # smart-paste
+        # starship
+        # sudo
+        # time-travel
+        # toggle-pane
+        # vcs-files
+        # wl-clipboard
+        # yatline-catppuccin
+        # yatline-githead
       ]
       ++ optionals (pkgs.stdenv.isDarwin) [mactag];
   };
