92dfae3cac679fe019fbb9fe0333a294ccacd849
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-04 01:27:14 -0500

    launchers

--- Packages/home/interface/vicinae/settings.nix
+++ Packages/home/interface/vicinae/settings.nix
@@ -2,9 +2,9 @@
   systemd = {
     enable = true;
     autoStart = true;
-    environment = {
-      USE_LAYER_SHELL = 1;
-    };
+    # environment = {
+    #   USE_LAYER_SHELL = 1;
+    # };
   };
   settings = {
     close_on_focus_loss = true;
