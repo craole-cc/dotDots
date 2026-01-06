a37ceb443bfa20ebbae1bd94746da92ea362885e
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-05 23:02:56 -0500

    caelestia

--- Packages/home/interface/caelestia/settings/default.nix
+++ Packages/home/interface/caelestia/settings/default.nix
@@ -1,9 +1,9 @@
 {mkMerge}: {
   settings = mkMerge [
     (import ./bar.nix {})
-    (import ./services.nix {})
     (import ./control.nix {})
     (import ./desktop.nix {})
+    (import ./services.nix {})
     # (import ./services.nix {})
     # (import ./services.nix {})
     # (import ./services.nix {})
