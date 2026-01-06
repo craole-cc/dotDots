80038ab4600bc81ea98fa9916879cae59d2fe871
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-05 19:10:28 -0500

    caelestia

--- Packages/home/interface/caelestia/bar.nix
+++ Packages/home/interface/caelestia/bar.nix
@@ -1,152 +1,101 @@
-{monitors, ...}: {
-  bar = {
-    backgroundOpacity = 0.95;
-    density = "default";
-    exclusive = true;
-    floating = false;
-    marginHorizontal = 1;
-    marginVertical = 0.06;
-    monitors = [monitors.all];
-    outerCorners = false;
-    position = "top";
-    showCapsule = false;
-    capsuleOpacity = 0;
-    showOutline = false;
-    useSeparateOpacity = false;
-    widgets = {
-      left = [
-        {
-          id = "Workspace";
-          labelMode = "index";
-          characterCount = 2;
-          colorizeIcons = true;
-          enableScrollWheel = true;
-          followFocusedScreen = true;
-          groupedBorderOpacity = 1;
-          hideUnoccupied = false;
-          iconScale = 0.8;
-          showApplications = true;
-          showLabelsOnlyWhenOccupied = true;
-          unfocusedIconsOpacity = .9;
-        }
-        {
-          hideMode = "alwaysExpanded";
-          icon = "rocket";
-          id = "CustomButton";
-          leftClickExec = "qs -c noctalia-shell ipc call launcher toggle";
-          leftClickUpdateText = false;
-          maxTextLength = {
-            horizontal = 10;
-            vertical = 10;
-          };
-          middleClickExec = "";
-          middleClickUpdateText = false;
-          parseJson = false;
-          rightClickExec = "";
-          rightClickUpdateText = false;
-          showIcon = true;
-          textCollapse = "";
-          textCommand = "";
-          textIntervalMs = 3000;
-          textStream = false;
-          wheelDownExec = "";
-          wheelDownUpdateText = false;
-          wheelExec = "";
-          wheelMode = "unified";
-          wheelUpExec = "";
-          wheelUpUpdateText = false;
-          wheelUpdateText = false;
-        }
-        {
-          id = "ActiveWindow";
-          colorizeIcons = false;
-          hideMode = "hidden";
-          maxWidth = 640;
-          scrollingMode = "hover";
-          showIcon = true;
-          useFixedWidth = false;
-        }
-      ];
-      center = [
-        {
-          customFont = "";
-          formatHorizontal = "HH:mm ddd; MMM dd";
-          formatVertical = "HH mm - dd MM";
-          id = "Clock";
-          tooltipFormat = "HH:mm ddd; MMM dd";
-          useCustomFont = false;
-          usePrimaryColor = false;
-        }
-        {
-          id = "MediaMini";
-          hideMode = "hidden";
-          hideWhenIdle = false;
-          maxWidth = 145;
-          scrollingMode = "hover";
-          showAlbumArt = true;
-          showArtistFirst = true;
-          showProgressRing = true;
-          showVisualizer = true;
-          useFixedWidth = true;
-          visualizerType = "linear";
-        }
-      ];
-      right = [
-        {
-          id = "Tray";
-          blacklist = [];
-          colorizeIcons = false;
-          drawerEnabled = true;
-          hidePassive = false;
-          pinned = [];
-        }
-        {
-          id = "SystemMonitor";
-          compactMode = true;
-          diskPath = "/";
-          showCpuTemp = true;
-          showCpuUsage = true;
-          showDiskUsage = true;
-          showGpuTemp = true;
-          showMemoryAsPercent = true;
-          showMemoryUsage = true;
-          showNetworkStats = true;
-          useMonospaceFont = true;
-          usePrimaryColor = true;
-        }
-        {id = "ScreenRecorder";}
-        {
-          hideWhenZero = false;
-          id = "NotificationHistory";
-          showUnreadBadge = true;
-        }
-        {
-          deviceNativePath = "";
-          displayMode = "onhover";
-          hideIfNotDetected = true;
-          id = "Battery";
-          showNoctaliaPerformance = false;
-          showPowerProfiles = false;
-          warningThreshold = 30;
-        }
-        {
-          displayMode = "onhover";
-          id = "Volume";
-        }
-        {
-          displayMode = "onhover";
-          id = "Brightness";
-        }
-        {
-          colorizeDistroLogo = false;
-          colorizeSystemIcon = "none";
-          customIconPath = "";
-          enableColorization = false;
-          icon = "noctalia";
-          id = "ControlCenter";
-          useDistroLogo = true;
-        }
-      ];
+{...}: {
+  settings.bar = {
+    persistent = true;
+    showOnHover = true;
+    dragThreshold = 20;
+    scrollActions = {
+      workspaces = true;
+      volume = true;
+      brightness = true;
     };
   };
+
+  popouts = {
+    activeWindow = true;
+    tray = true;
+    statusIcons = true;
+  };
+
+  workspaces = {
+    shown = 5;
+    activeIndicator = true;
+    occupiedBg = false;
+    showWindows = true;
+    showWindowsOnSpecialWorkspaces = true;
+    activeTrail = false;
+    perMonitorWorkspaces = true;
+    label = "  ";
+    occupiedLabel = "󰮯";
+    activeLabel = "󰮯";
+    capitalisation = "preserve";
+    specialWorkspaceIcons = [];
+  };
+
+  tray = {
+    background = false;
+    recolour = true;
+    compact = true;
+    iconSubs = [];
+  };
+
+  status = {
+    showAudio = true;
+    showMicrophone = true;
+    showKbLayout = false;
+    showNetwork = true;
+    showBluetooth = true;
+    showBattery = true;
+    showLockStatus = true;
+  };
+
+  clock = {
+    showIcon = true;
+  };
+
+  sizes = {
+    innerWidth = 40;
+    windowPreviewSize = 400;
+    trayMenuWidth = 300;
+    batteryWidth = 250;
+    networkWidth = 320;
+  };
+
+  entries = [
+    {
+      enabled = true;
+      id = "logo";
+    }
+    {
+      enabled = true;
+      id = "workspaces";
+    }
+    {
+      enabled = true;
+      id = "spacer";
+    }
+    {
+      enabled = true;
+      id = "activeWindow";
+    }
+    {
+      enabled = true;
+      id = "spacer";
+    }
+    {
+      enabled = true;
+      id = "tray";
+    }
+    {
+      enabled = true;
+      id = "clock";
+    }
+    {
+      enabled = true;
+      id = "statusIcons";
+    }
+    {
+      enabled = true;
+      id = "power";
+    }
+  ];
 }
--- Packages/home/interface/caelestia/default.nix
+++ Packages/home/interface/caelestia/default.nix
@@ -27,6 +27,7 @@
     enable = true;
     programs.${name} = mkMerge [
       (import ./cli.nix {})
+      (import ./bar.nix {})
       # (import ./settings.nix {inherit pkgs;})
     ];
     home = {};
@@ -36,50 +37,3 @@ in {
     inherit (cfg) programs home;
   };
 }
-#   config,
-#   nixosConfig,
-#   host,
-#   lib,
-#   lix,
-#   # pkgs,
-#   user,
-#   ...
-# }: let
-#   app = "caelestia";
-#   inherit (lib.modules) mkIf mkMerge;
-#   inherit (lix.lists.predicates) isIn;
-#   inherit (lix.hardware.display) getNames getPrimaryName;
-#   desired = user.interface.bar or null;
-#   primary = desired != null;
-#   allowed = isIn app (user.applications.allowed or []);
-#   enable = (primary || allowed) && config.programs ? ${app};
-#   monitors = {
-#     all = getNames {inherit host;};
-#     primary = getPrimaryName {inherit host;};
-#     # TODO: We need to store wallpaper path in monitors
-#   };
-#   homeDir = config.home.homeDirectory;
-#   terminal = user.applications.terminal.primary;
-# in {
-#   config = mkIf enable {
-#     programs.${app} = mkMerge [
-#       {
-#         inherit enable;
-#         settings = mkMerge [
-#           (import ./cli.nix {})
-#           # (import ./bar.nix {inherit monitors;})
-#           # (import ./color.nix {})
-#           # (import ./control.nix {inherit terminal;})
-#           # (import ./desktop.nix {inherit monitors homeDir;})
-#           # (import ./general.nix {inherit lib config nixosConfig;})
-#           # (import ./info.nix {inherit host monitors;})
-#           # (import ./output.nix {inherit homeDir;})
-#         ];
-#       }
-#     ];
-#     home = {
-#       sessionVariables.BAR = desired;
-#       shellAliases = mkIf primary {bar = "${app} &";};
-#     };
-#   };
-# }
