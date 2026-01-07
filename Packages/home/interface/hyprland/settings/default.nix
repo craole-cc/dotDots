675ce75a394b6a57b5fda5a2c0c8e3da1beedd1d
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-07 01:55:20 -0500

    hyprland

--- Packages/home/interface/hyprland/settings/default.nix
+++ Packages/home/interface/hyprland/settings/default.nix
@@ -8,7 +8,7 @@
   settings = mkMerge [
     (import ./io.nix {inherit host lib lix;})
     # (import ./startup.nix)
-    (import ./core.nix)
-    (import ./rules.nix {inherit lib;})
+    # (import ./core.nix)
+    # (import ./rules.nix {inherit lib;})
   ];
 }
