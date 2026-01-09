ae5581b138fe27f926f77b19ba3eede3547fdbd1
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-09 17:14:19 -0500

    tried gnome

--- Packages/home/interface/plasma/default.nix
+++ Packages/home/interface/plasma/default.nix
@@ -15,25 +15,24 @@
   inherit (lib.modules) mkIf mkMerge;
   inherit (lix.lists.predicates) isIn;
   isAllowed = isIn (user.interface.desktopEnvironment or null) opt;
-
-  # Check if the OPTION is defined (i.e., plasma-manager module is imported)
   isAvailable = options ? programs.${app};
 
   packages = import ./packages.nix {inherit pkgs;};
-in {
-  config = mkIf (isAvailable && isAllowed) {
-    programs.${app} = mkMerge [
-      {enable = true;}
-      (import ./bindings)
-      # // import ./files
-      (import ./modules {inherit src pkgs config nixosConfig;})
-    ];
+in
+  lib.optionalAttrs isAvailable {
+    config = mkIf isAllowed {
+      programs.${app} = mkMerge [
+        {enable = true;}
+        (import ./bindings)
+        # // import ./files
+        (import ./modules {inherit src pkgs config nixosConfig;})
+      ];
 
-    home = {
-      shellAliases = {
-        plasma-config-dump = "nix run github:nix-community/plasma-manager > $DOTS/Packages/home/interface/plasma/dump.nix";
+      home = {
+        shellAliases = {
+          plasma-config-dump = "nix run github:nix-community/plasma-manager > $DOTS/Packages/home/interface/plasma/dump.nix";
+        };
+        inherit packages;
       };
-      inherit packages;
     };
-  };
-}
+  }
