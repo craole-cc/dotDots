ba658d687d41f109269a09b5eb613b4ef0e13d04
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-09 17:15:20 -0500

    tried gnome

--- Packages/home/interface/plasma/default.nix
+++ Packages/home/interface/plasma/default.nix
@@ -5,7 +5,6 @@
   config,
   nixosConfig,
   pkgs,
-  options,
   src,
   ...
 }: let
@@ -15,24 +14,25 @@
   inherit (lib.modules) mkIf mkMerge;
   inherit (lix.lists.predicates) isIn;
   isAllowed = isIn (user.interface.desktopEnvironment or null) opt;
-  isAvailable = options ? programs.${app};
+
+  # Check if the option is defined by checking config._module.args or simply trying
+  # This won't cause infinite recursion
+  isAvailable = builtins.hasAttr app (config.programs or {});
 
   packages = import ./packages.nix {inherit pkgs;};
 in
-  lib.optionalAttrs isAvailable {
-    config = mkIf isAllowed {
-      programs.${app} = mkMerge [
-        {enable = true;}
-        (import ./bindings)
-        # // import ./files
-        (import ./modules {inherit src pkgs config nixosConfig;})
-      ];
+  lib.optionalAttrs (isAllowed && isAvailable) {
+    programs.${app} = mkMerge [
+      {enable = true;}
+      (import ./bindings)
+      # // import ./files
+      (import ./modules {inherit src pkgs config nixosConfig;})
+    ];
 
-      home = {
-        shellAliases = {
-          plasma-config-dump = "nix run github:nix-community/plasma-manager > $DOTS/Packages/home/interface/plasma/dump.nix";
-        };
-        inherit packages;
+    home = {
+      shellAliases = {
+        plasma-config-dump = "nix run github:nix-community/plasma-manager > $DOTS/Packages/home/interface/plasma/dump.nix";
       };
+      inherit packages;
     };
   }
