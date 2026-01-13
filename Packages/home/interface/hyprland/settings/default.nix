b773a46fcf9de820c38f9f8644588d79defc4b7d
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-13 18:35:07 -0500

    hyprland

--- Packages/home/interface/hyprland/settings/default.nix
+++ Packages/home/interface/hyprland/settings/default.nix
@@ -8,41 +8,6 @@
 }: let
   inherit (lib.strings) hasInfix toUpper;
 
-  mkCmd = category: field: default: let
-    apps = user.applications or {};
-    name = apps.${category}.${field} or default;
-  in
-    if category == "terminal"
-    then
-      if name == "foot"
-      then "footclient"
-      else name
-    else if category == "browser"
-    then
-      if hasInfix "zen" name
-      then
-        if hasInfix "twilight" name
-        then "zen-twilight"
-        else "zen-beta"
-      else if hasInfix "edge" name
-      then "microsoft-edge"
-      else name
-    else if category == "editor"
-    then
-      if hasInfix "code" name
-      then "code"
-      else if hasInfix "zed" name
-      then "zeditor"
-      else name
-    else if category == "launcher"
-    then
-      if name == "vicinae"
-      then "vicinae toggle"
-      else if name == "fuzzel"
-      then "pkill fuzzel || fuzzel --list-executables-in-path"
-      else name
-    else name;
-
   args = {
     inherit
       host
@@ -57,22 +22,93 @@
         "Super"
     );
 
-    cmd = {
+    cmd = let
+      mkCmd = {
+        category,
+        field,
+        default,
+      }: let
+        apps = user.applications or {};
+        name = apps.${category}.${field} or default;
+      in
+        if category == "terminal"
+        then
+          if name == "foot"
+          then "footclient"
+          else name
+        else if category == "browser"
+        then
+          if hasInfix "zen" name
+          then
+            if hasInfix "twilight" name
+            then "zen-twilight"
+            else "zen-beta"
+          else if hasInfix "edge" name
+          then "microsoft-edge"
+          else name
+        else if category == "editor"
+        then
+          if hasInfix "code" name
+          then "code"
+          else if hasInfix "zed" name
+          then "zeditor"
+          else name
+        else if category == "launcher"
+        then
+          if name == "vicinae"
+          then "vicinae toggle"
+          else if name == "fuzzel"
+          then "pkill fuzzel || fuzzel --list-executables-in-path"
+          else name
+        else name;
+    in {
       browser = {
-        primary = mkCmd "browser" "primary" "zen-twilight";
-        secondary = mkCmd "browser" "secondary" "microsoft-edge";
+        primary = mkCmd {
+          category = "browser";
+          field = "primary";
+          default = "zen-twilight";
+        };
+        secondary = mkCmd {
+          category = "browser";
+          field = "secondary";
+          default = "microsoft-edge";
+        };
       };
       editor = {
-        primary = mkCmd "editor" "gui.primary" "vscode";
-        secondary = mkCmd "editor" "gui.secondary" "zed";
+        primary = mkCmd {
+          category = "editor";
+          field = "gui.primary";
+          default = "vscode";
+        };
+        secondary = mkCmd {
+          category = "editor";
+          field = "gui.secondary";
+          default = "zed";
+        };
       };
       launcher = {
-        primary = mkCmd "launcher" "primary" "vicinae";
-        secondary = mkCmd "launcher" "secondary" "fuzzel";
+        primary = mkCmd {
+          category = "launcher";
+          field = "primary";
+          default = "vicinae";
+        };
+        secondary = mkCmd {
+          category = "launcher";
+          field = "secondary";
+          default = "fuzzel";
+        };
       };
       terminal = {
-        primary = mkCmd "terminal" "primary" "foot";
-        secondary = mkCmd "terminal" "secondary" "ghostty";
+        primary = mkCmd {
+          category = "terminal";
+          field = "primary";
+          default = "foot";
+        };
+        secondary = mkCmd {
+          category = "terminal";
+          field = "secondary";
+          default = "ghostty";
+        };
       };
     };
 
