b86dc8ce7f5f27afb45cab1433b6ede6e040c370
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-13 12:00:52 -0500

    hyprland

--- Packages/home/interface/hyprland/settings/workspaces.nix
+++ Packages/home/interface/hyprland/settings/workspaces.nix
@@ -1,68 +1,41 @@
 {lib, ...}: let
-  cat = lib.lists.concatMap;
-  mat = lib.attrsets.mapAttrsToList;
+  inherit (lib.lists) concatMap flatten;
+  inherit (lib.attrsets) mapAttrsToList;
 
-  workspaces = [
-    "0"
-    "1"
-    "2"
-    "3"
-    "4"
-    "5"
-    "6"
-    "7"
-    "8"
-    "9"
-    "F1"
-    "F2"
-    "F3"
-    "F4"
-    "F5"
-    "F6"
-    "F7"
-    "F8"
-    "F9"
-    "F10"
-    "F11"
-    "F12"
-  ];
+  workspaces = map toString (lib.range 0 9) ++ map (n: "F${toString n}") (lib.range 1 12);
 
-  directions = rec {
+  directions = {
     left = "l";
     right = "r";
     up = "u";
     down = "d";
-    h = left;
-    l = right;
-    k = up;
-    j = down;
+    h = "l";
+    l = "r";
+    k = "u";
+    j = "d";
   };
 
-  # Helper to create special workspace configs
-  mkSpecialWorkspace = app: config: {
-    primary = {
-      bind = "$MOD, ${config.key}, togglespecialworkspace, ${config.primary}";
-      exec = "[workspace special:${config.primary} silent] $${app}";
-      rules = [
-        "workspace special:${config.primary}, class:^($${app})$"
-        "size 100% ${config.size}, workspace:^(${config.primary})$"
-        "move 0% 0%, workspace:^(${config.primary})$"
-        "float, workspace:^(${config.primary})$"
-        "noborder, workspace:^(${config.primary})$"
-      ];
-    };
-    secondary = {
-      bind = "$MOD SHIFT, ${config.key}, togglespecialworkspace, ${config.secondary}";
-      exec = "[workspace special:${config.secondary} silent] $${app}Alt";
-      rules = [
-        "workspace special:${config.secondary}, class:^($${app}Alt)$"
-        "size 100% ${config.size}, workspace:^(${config.secondary})$"
-        "move 0% 0%, workspace:^(${config.secondary})$"
-        "float, workspace:^(${config.secondary})$"
-        "noborder, workspace:^(${config.secondary})$"
-      ];
-    };
+  mkWorkspaceVariant = app: workspace: key: size: modifier: {
+    bind = "$MOD ${modifier}, ${key}, togglespecialworkspace, ${workspace}";
+    exec = "[workspace special:${workspace} silent] $${app}";
+    rules = [
+      "workspace special:${workspace}, class:^($${app})$"
+      "size 100% ${size}, workspace:^(${workspace})$"
+      "move 0% 0%, workspace:^(${workspace})$"
+      "float, workspace:^(${workspace})$"
+      "noborder, workspace:^(${workspace})$"
+    ];
   };
+
+  mkSpecialWorkspace = app: {
+    key,
+    primary,
+    secondary,
+    size,
+  }: [
+    (mkWorkspaceVariant app primary key size "")
+    (mkWorkspaceVariant "${app}Alt" secondary key size "SHIFT")
+  ];
 
   specialWorkspaces = {
     terminal = {
@@ -85,27 +58,25 @@
     };
   };
 
-  #~@ Generate all special workspace configs
-  specialConfigs = mat mkSpecialWorkspace specialWorkspaces;
+  allVariants = flatten (mapAttrsToList mkSpecialWorkspace specialWorkspaces);
+  mkDirectionalBinds = modifier: action:
+    mapAttrsToList (key: dir: "${modifier},${key},${action},${dir}") directions;
 in {
-  bind =
+  exec-once = map (v: v.exec) allVariants;
+  windowrulev2 = concatMap (v: v.rules) allVariants;
+  bind = flatten [
     #~@ Special workspaces
-    (cat (c: [c.primary.bind c.secondary.bind]) specialConfigs)
-    #~@ Regular workspace switching
-    ++ (map (n: "$MOD,${n},workspace,name:${n}") workspaces)
-    ++ (map (n: "$MOD SHIFT,${n},movetoworkspacesilent,name:${n}") workspaces)
+    (map (v: v.bind) allVariants)
+
+    #~@ Regular workspaces
+    (map (n: "$MOD,${n},workspace,name:${n}") workspaces)
+    (map (n: "$MOD SHIFT,${n},movetoworkspacesilent,name:${n}") workspaces)
+
     #~@ Directional bindings
-    ++ (mat (key: dir: "$MOD,${key},movefocus,${dir}") directions)
-    ++ (mat (key: dir: "$MOD SHIFT,${key},swapwindow,${dir}") directions)
-    ++ (mat (key: dir: "$MOD CONTROL,${key},movewindoworgroup,${dir}") directions)
-    ++ (mat (key: dir: "$MOD ALT,${key},focusmonitor,${dir}") directions)
-    ++ (mat (key: dir: "$MOD ALT SHIFT,${key},movecurrentworkspacetomonitor,${dir}") directions);
-
-  exec-once =
-    (map (c: c.primary.exec) specialConfigs)
-    ++ (map (c: c.secondary.exec) specialConfigs);
-
-  windowrulev2 =
-    (cat (c: c.primary.rules) specialConfigs)
-    ++ (cat (c: c.secondary.rules) specialConfigs);
+    (mkDirectionalBinds "$MOD" "movefocus")
+    (mkDirectionalBinds "$MOD SHIFT" "swapwindow")
+    (mkDirectionalBinds "$MOD CONTROL" "movewindoworgroup")
+    (mkDirectionalBinds "$MOD ALT" "focusmonitor")
+    (mkDirectionalBinds "$MOD ALT SHIFT" "movecurrentworkspacetomonitor")
+  ];
 }
