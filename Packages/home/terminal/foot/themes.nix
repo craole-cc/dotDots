aa84455b87112aded9811d6cea9e193d586394b9
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-01-14 11:58:10 -0500

    updated feet

--- Packages/home/terminal/foot/themes.nix
+++ Packages/home/terminal/foot/themes.nix
@@ -1,60 +1,60 @@
 let
-  alpha = 0.85;
+  alpha = 0.95;
 in {
   #~@ Primary Color Theme (Dark - Catppuccin Frappe)
   #? Theme 1, accessed via initial-color-theme=1
   colors = {
     inherit alpha;
-    background = "303446";
-    foreground = "c6d0f5";
+    # background = "303446";
+    # foreground = "c6d0f5";
 
-    #~@ Regular Colors (0-7)
-    regular0 = "51576d";
-    regular1 = "e78284";
-    regular2 = "a6d189";
-    regular3 = "e5c890";
-    regular4 = "8caaee";
-    regular5 = "f4b8e4";
-    regular6 = "81c8be";
-    regular7 = "b5bfe2";
+    # #~@ Regular Colors (0-7)
+    # regular0 = "51576d";
+    # regular1 = "e78284";
+    # regular2 = "a6d189";
+    # regular3 = "e5c890";
+    # regular4 = "8caaee";
+    # regular5 = "f4b8e4";
+    # regular6 = "81c8be";
+    # regular7 = "b5bfe2";
 
-    #~@ Bright Colors (8-15)
-    bright0 = "626880";
-    bright1 = "e78284";
-    bright2 = "a6d189";
-    bright3 = "e5c890";
-    bright4 = "8caaee";
-    bright5 = "f4b8e4";
-    bright6 = "81c8be";
-    bright7 = "a5adce";
+    # #~@ Bright Colors (8-15)
+    # bright0 = "626880";
+    # bright1 = "e78284";
+    # bright2 = "a6d189";
+    # bright3 = "e5c890";
+    # bright4 = "8caaee";
+    # bright5 = "f4b8e4";
+    # bright6 = "81c8be";
+    # bright7 = "a5adce";
   };
 
   #~@ Alternative Color Theme (Light - Catppuccin Latte)
   #? Theme 2, accessed via initial-color-theme=2
   colors2 = {
     inherit alpha;
-    background = "eff1f5";
-    foreground = "4c4f69";
+    # background = "eff1f5";
+    # foreground = "4c4f69";
 
-    #~@ Regular Colors (0-7)
-    regular0 = "5c5f77";
-    regular1 = "d20f39";
-    regular2 = "40a02b";
-    regular3 = "df8e1d";
-    regular4 = "1e66f5";
-    regular5 = "ea76cb";
-    regular6 = "179299";
-    regular7 = "acb0be";
+    # #~@ Regular Colors (0-7)
+    # regular0 = "5c5f77";
+    # regular1 = "d20f39";
+    # regular2 = "40a02b";
+    # regular3 = "df8e1d";
+    # regular4 = "1e66f5";
+    # regular5 = "ea76cb";
+    # regular6 = "179299";
+    # regular7 = "acb0be";
 
-    #~@ Bright Colors (8-15)
-    bright0 = "6c6f85";
-    bright1 = "d20f39";
-    bright2 = "40a02b";
-    bright3 = "df8e1d";
-    bright4 = "1e66f5";
-    bright5 = "ea76cb";
-    bright6 = "179299";
-    bright7 = "bcc0cc";
+    # #~@ Bright Colors (8-15)
+    # bright0 = "6c6f85";
+    # bright1 = "d20f39";
+    # bright2 = "40a02b";
+    # bright3 = "df8e1d";
+    # bright4 = "1e66f5";
+    # bright5 = "ea76cb";
+    # bright6 = "179299";
+    # bright7 = "bcc0cc";
   };
 
   #~@ Cursor Configuration
