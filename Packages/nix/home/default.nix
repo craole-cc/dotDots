8afd9ae71ed4130885c0a17ff862001eda65ee55
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-03-03 17:48:19 -0500

    HYPRLAND v0.53+ SYNTAX UPDATE

--- Packages/nix/home/default.nix
+++ Packages/nix/home/default.nix
@@ -1,9 +1,1 @@
-{lix, ...}: {
-  imports = lix.filesystem.importers.importAll ./.;
-  # imports = lix.filesystem.importers.importAll ./browser;
-  # imports = [
-  #   ./browser
-  #   ./editor
-  #   ./common
-  # ];
-}
+{lix, ...}: {imports = lix.filesystem.importers.importAll ./.;}
