8f70034e4e65730a11a8b5ff6ce7ea36232ce0b4
Craole <32288735+Craole@users.noreply.github.com>
2026-03-15 20:12:25 -0500

    build successful but apps not fullscreen

--- Modules/nix/home/interface/manager/hyprland/settings/default.nix
+++ Modules/nix/home/interface/manager/hyprland/settings/default.nix
@@ -7,11 +7,13 @@
   mkMerge,
   withRules ? true,
   ...
-}: {
+}: let
+  inherit (lib.modules) mkIf;
+in {
   settings = mkMerge [
     (import ./core.nix {inherit apps keyboard;})
     (import ./io.nix {inherit apps host lix lib keyboard;})
-    (lib.mkIf withRules (import ./rules.nix {inherit lib;}))
-    (import ./workspaces.nix {inherit lib apps keyboard;})
+    (mkIf withRules (import ./rules.nix {inherit lib;}))
+    (mkIf withRules (import ./workspaces.nix {inherit lib apps keyboard;}))
   ];
 }
