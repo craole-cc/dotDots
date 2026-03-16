eee87c0df4849163f9098194b6eab050ae3040ce
Craole <32288735+Craole@users.noreply.github.com>
2026-03-15 22:19:23 -0500

    default apps floating

--- Modules/nix/home/interface/manager/hyprland/settings/rules/gaps.nix
+++ Modules/nix/home/interface/manager/hyprland/settings/rules/gaps.nix
@@ -1,9 +1,9 @@
 {
   #> Remove borders/rounding when only one tiled window
-  # workspace = [
-  #   "w[tv1], gapsout:0, gapsin:0"
-  #   "f[1], gapsout:0, gapsin:0"
-  # ];
+  workspace = [
+    "w[tv1], gapsout:0, gapsin:0, bordersize:0"
+    "f[1], gapsout:0, gapsin:0, bordersize:0"
+  ];
 
   windowrule = [
     "border_size 0, match:float 0, match:workspace w[tv1]s[false]"
