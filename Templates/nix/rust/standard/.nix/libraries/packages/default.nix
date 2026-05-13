1fab070d70eff6f72d782ae5d38ab1375783bae8
Craole <32288735+Craole@users.noreply.github.com>
2026-05-13 10:02:01 -0500

    rust-standard package modules

--- Templates/nix/rust/standard/.nix/libraries/packages/default.nix
+++ Templates/nix/rust/standard/.nix/libraries/packages/default.nix
@@ -2,6 +2,6 @@
 lib.assembly.importLibs {
   inherit lib;
   path = ./.;
-  priority = ["systems.nix" "sources.nix" "binaries.nix"];
-  ignore = ["openclaw.nix"];
+  # priority = ["systems.nix" "sources.nix" "binaries.nix"];
+  # ignore = ["openclaw.nix"];
 }
