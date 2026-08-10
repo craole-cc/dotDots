1acc211984c307e6bf244d6358e1f2820b0a5d80
Craole <32288735+Craole@users.noreply.github.com>
2026-08-10 05:26:12 +0000

    stage Hyprland config and remove legacy Lua file

--- Modules/nix/home/interface/manager/hyprland/default.nix
+++ Modules/nix/home/interface/manager/hyprland/default.nix
@@ -19,6 +19,7 @@
   inherit (user.interface) windowManager;
 
   inherit (lib.modules) mkIf mkMerge;
+  inherit (lix.modules.core._) mkStaged;
   inherit (lib.options) mkEnableOption mkOption;
   inherit (lib.types) bool;
 
@@ -29,6 +30,40 @@
     ).${
       target
     };
+
+  payload = {
+    wayland.windowManager.hyprland = mkMerge [
+      {
+        enable = true;
+        configType = "hyprlang";
+        plugins = [];
+      }
+      (import ./settings {
+        inherit
+          host
+          lib
+          lix
+          apps
+          user
+          keyboard
+          mkMerge
+          ;
+        inherit (cfg) withRules;
+        keys = cfgTop.interface.keyboard;
+      })
+      (import ./submaps {inherit mkMerge;})
+    ];
+
+    programs = mkAddons "programs";
+    services = mkAddons "services";
+
+    home.activation.removeLegacyHyprlandLua = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      legacy="$HOME/.config/hypr/hyprland.lua"
+      if [ -e "$legacy" ] || [ -L "$legacy" ]; then
+        rm -f "$legacy"
+      fi
+    '';
+  };
 in {
   options.${top}.inputs.${dom}.${mod} = {
     enable = mkEnableOption mod // {default = windowManager == "hyprland";};
@@ -40,58 +75,8 @@ in {
     withRules = mkEnableOption "Window rules" // {default = true;};
   };
 
-config = lib.mkMerge [
-    (mkIf cfg.enable {
-    wayland.windowManager.hyprland = mkMerge [
-      {
-        enable = true;
-        configType = "hyprlang";
-        plugins = [];
-      }
-      (import ./settings {
-        inherit
-          host
-          lib
-          lix
-          apps
-          user
-          keyboard
-          mkMerge
-          ;
-        inherit (cfg) withRules;
-        keys = cfgTop.interface.keyboard;
-      })
-      (import ./submaps {inherit mkMerge;})
-    ];
-
-    programs = mkAddons "programs";
-    services = mkAddons "services";
-  })
-    {${top}.output = mkIf cfg.enable {
-    wayland.windowManager.hyprland = mkMerge [
-      {
-        enable = true;
-        configType = "hyprlang";
-        plugins = [];
-      }
-      (import ./settings {
-        inherit
-          host
-          lib
-          lix
-          apps
-          user
-          keyboard
-          mkMerge
-          ;
-        inherit (cfg) withRules;
-        keys = cfgTop.interface.keyboard;
-      })
-      (import ./submaps {inherit mkMerge;})
-    ];
-
-    programs = mkAddons "programs";
-    services = mkAddons "services";
-  };}
-  ];
+  config = lib.mkMerge (mkStaged {
+    inherit top payload;
+    condition = cfg.enable;
+  });
 }
