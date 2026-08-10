b5770b7c0d68963e4d694ce1aed95e3576836359
Craole <32288735+Craole@users.noreply.github.com>
2026-08-10 05:01:15 +0000

    refactor simple home modules through staging helper

--- Modules/nix/home/vsc/git.nix
+++ Modules/nix/home/vsc/git.nix
@@ -1,28 +1,22 @@
-{config, top, user, ...}: {
-  programs.git = {
-    enable = config.${top}.inputs.applications.utilities.git.enable;
-    lfs.enable = true;
-    settings = {
-      user = {
-        name = user.git.name or null;
-        email = user.git.email or null;
-      };
-      core = {
-        whitespace = "trailing-space,space-before-tab";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      url = {
-        "https://github.com/" = {
-          insteadOf = [
-            "gh:"
-            "github:"
-          ];
+{config, lib, lix, top, user, ...}: let
+  inherit (lix.modules.core._) mkStaged;
+
+  payload = {
+    programs.git = {
+      enable = config.${top}.inputs.applications.utilities.git.enable;
+      lfs.enable = true;
+      settings = {
+        user = {
+          name = user.git.name or null;
+          email = user.git.email or null;
         };
+        core.whitespace = "trailing-space,space-before-tab";
+        init.defaultBranch = "main";
+        url."https://github.com/".insteadOf = ["gh:" "github:"];
       };
+      includes = [];
     };
-    includes = [];
   };
-  ${top}.output.programs.git = config.programs.git;
+in {
+  config = lib.mkMerge (mkStaged {inherit top payload;});
 }
