c7157ec891e45f33b5672108bdb35e979f1d3414
Craole <32288735+Craole@users.noreply.github.com>
2026-08-23 21:37:50 +0000

    fix(darkman): use runtime wallman command

--- Modules/nix/home/interface/components/darkman/default.nix
+++ Modules/nix/home/interface/components/darkman/default.nix
@@ -73,7 +73,7 @@
         cmdDconf = "${pkgs.dconf}/bin/dconf";
         cmdNotify = "${pkgs.libnotify}/bin/notify-send";
         cmdSd = "${pkgs.sd}/bin/sd";
-        cmdWallman = "${paths.wallpapers.manager}/bin/wallman";
+        cmdWallman = "wallman";
         cmdThemeToggle = "theme-toggle";
       };
       name = "nixos-theme";
