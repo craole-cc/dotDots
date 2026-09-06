a6022903196ee9fdd36c01b79cbe6aeb11ee5354
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-06 14:57:12 -0500

    ai: make Hindsight the active memory backend

--- Modules/nix/global/ai/memory/default.nix
+++ Modules/nix/global/ai/memory/default.nix
@@ -1,14 +1,3 @@
-args: let
-  mem0 = import ./mem0 args;
+args: {
   hindsight = import ./hindsight args;
-in {
-  inherit (mem0) mem0 status verify;
-  inherit hindsight;
-
-  packages = mem0.packages ++ hindsight.packages;
-  env = mem0.env // hindsight.env;
-  shellHook = ''
-    ${mem0.shellHook}
-    ${hindsight.shellHook}
-  '';
 }
