b9a8f6404c13541c6390fd9398e34c3a3f8981f7
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-06 20:42:22 -0500

    ai: isolate Hindsight preset ports

--- Modules/nix/global/ai/presets/hermes/hindsight.nix
+++ Modules/nix/global/ai/presets/hermes/hindsight.nix
@@ -13,6 +13,11 @@ import ./lib.nix {
   init = ''
     export HINDSIGHT_DATA_DIR="$AI_HOME/hindsight"
     export HINDSIGHT_SECRETS_FILE="''${HINDSIGHT_SECRETS_FILE:-''${PRIVATE:-$HOME/Private}/hindsight.env}"
+    export HINDSIGHT_BIND_ADDRESS="$AI_BIND_ADDRESS"
+    export HINDSIGHT_API_PORT="$((8888 + AI_PORT_OFFSET))"
+    export HINDSIGHT_MCP_PORT="$((9999 + AI_PORT_OFFSET))"
+    export HINDSIGHT_UI_PORT="$((8889 + AI_PORT_OFFSET))"
+    export HINDSIGHT_API_URL="http://$HINDSIGHT_BIND_ADDRESS:$HINDSIGHT_API_PORT"
     export HINDSIGHT_MODE="''${HINDSIGHT_MODE:-local_external}"
     export HINDSIGHT_BANK_ID="''${HINDSIGHT_BANK_ID:-hermes}"
     export HINDSIGHT_RECALL_BUDGET="''${HINDSIGHT_RECALL_BUDGET:-mid}"
