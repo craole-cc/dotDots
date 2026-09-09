9e96b365e0dda3a3b76972da118e9e9328035f86
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-09 07:19:25 -0500

    fix(core): map API password hashes correctly

--- Modules/nix/core/users/default.nix
+++ Modules/nix/core/users/default.nix
@@ -62,7 +62,7 @@ in
             isNormalUser = user.role != "service";
             isSystemUser = user.role == "service";
             description = user.description or name;
-            password = user.password or null;
+            hashedPassword = user.password or null;
             group = name;
             extraGroups =
               optionals (user.role != "service") ["users"]
