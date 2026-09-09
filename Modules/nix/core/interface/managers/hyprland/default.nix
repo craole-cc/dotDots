d1de02731e8e3443151055c83cf00c3520215700
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-09 02:52:45 -0500

    fix(core): gate Hyprland by selected manager

--- Modules/nix/core/interface/managers/hyprland/default.nix
+++ Modules/nix/core/interface/managers/hyprland/default.nix
@@ -17,7 +17,7 @@ in
   mkConfig {
     inherit context;
     options = {
-      enable = mkEnable {inherit context;} // ctx.wantsHyprland;
+      enable = mkEnable ({inherit context;} // ctx.wantsHyprland);
     };
     outputs = {
       programs.hyprland = {
