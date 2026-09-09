3542e66728221bcb96d4c4a71e09746286468ead
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-09 09:32:58 -0500

    fix(cache): use current Chaotic Nyx cache

--- API/nix/hosts/Victus/default.nix
+++ API/nix/hosts/Victus/default.nix
@@ -27,6 +27,11 @@ in {
     kernel = "linuxPackages_cachyos-lto";
   };
 
+  caches.nyx = {
+    sub = "https://nyx-cache.chaotic.cx/";
+    key = "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk=";
+  };
+
   modules = [
     "nvme"
     "xhci_pci"
