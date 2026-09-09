97eae0cf5dcfa5ed59d17be7a4fab418857651e9
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-09 06:51:37 -0500

    fix(core): wire selected DMS greeter

---
+++ Modules/nix/core/interface/common/greeter.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  host,
+  lix,
+  ...
+}: let
+  context = mkContext {
+    inherit config;
+    dom = "interface";
+    sub = "common";
+    mod = "greeter";
+  };
+  inherit (context) cfg;
+
+  inherit (lix.modules.construction) mkConfig mkContext;
+  inherit (lix.options.construction) literalExpression mkEnable mkOption;
+  inherit (lix.types.combinators) nullOr;
+  inherit (lix.types.primitives) str;
+
+  interface = host.interface or {};
+  selected = interface.displayManager or null;
+  compositor = interface.compositor.window or interface.compositor.desktop or null;
+in
+  mkConfig {
+    inherit context;
+    options = {
+      enable = mkEnable {
+        inherit context;
+        condition = selected == "dms-greeter";
+        defaultText = literalExpression ''host.interface.displayManager == "dms-greeter"'';
+      };
+
+      compositor = mkOption {
+        description = "Compositor used by the DMS greeter.";
+        default = compositor;
+        defaultText = literalExpression "host.interface.compositor.window or host.interface.compositor.desktop or null";
+        type = nullOr str;
+      };
+    };
+
+    outputs = {
+      services.displayManager.dms-greeter = {
+        enable = cfg.enable;
+        compositor.name = cfg.compositor;
+      };
+    };
+  }
