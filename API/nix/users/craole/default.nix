1394685d7e7d11fe374f217d38f78712a390a856
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-10 15:01:58 -0500

    feat(user): request Foot terminal

--- API/nix/users/craole/default.nix
+++ API/nix/users/craole/default.nix
@@ -41,6 +41,24 @@
       # modifier = "SUPER";
       swapCapsEscape = false;
       vimKeybinds = false;
+
+      # Special-workspace defaults live in the keyboard schema. Override only
+      # the pieces that differ for this user; omitted fields inherit defaults.
+      # bindings = {
+      #   specialTerminal = {
+      #     mod = "SUPER";
+      #     key = "grave";
+      #   };
+      #   specialEditor = {
+      #     mod = "SUPER SHIFT";
+      #     key = "grave";
+      #   };
+      #   specialBrowser = {
+      #     mod = "SUPER CTRL";
+      #     key = "grave";
+      #   };
+      # };
+
       #~@ Keybindings Map
       #? Define application/action keys agnostic of WM/DE
       # bindings = {
@@ -167,6 +185,7 @@
     allowed = [
       # "atuin"
       "fastfetch"
+      "foot"
       "freetube"
       "warp-terminal"
       "jujutsu"
