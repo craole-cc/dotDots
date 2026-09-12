7b0952ce1d7aee71cdabe5748738e457e3d02dd5
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-11 20:36:13 -0500

    refactor(appearance): keep DMS contract in the staged module

--- Modules/nix/home/interface/config.nix
+++ Modules/nix/home/interface/config.nix
@@ -1,92 +1,67 @@
 {
-  config,
-  lib,
-  lix,
   pkgs,
-  user,
+  lib,
+  config,
+  lix,
+  top,
   ...
 }: let
+  dom = "interface";
+  cfg = config.${top}.resolved.${dom};
   inherit (lib.modules) mkForce;
-  inherit (lix.modules.construction) mkConfig mkContext;
-  inherit (lix.options.construction) mkEnable mkOption;
-  inherit (lix.styles.cursors) mkCursor;
-  inherit (lix.styles.icons) mkIcon;
+  # inherit (lib.strings) toLower;
+  getPackage = lix.attrsets.resolution.package;
+  # get = lix.attrsets.resolution.get;
 
-  context = mkContext {
-    inherit config;
-    dom = "interface";
-    sub = "style";
-    mod = "appearance";
-  };
+  #~@ Style configuration from user API
+  # style = user.interface.style or {};
+  # current = style.current or "dark";
 
-  style = user.style or {};
-  theme = style.theme or {};
-  polarity = theme.polarity or "dark";
-
-  cursorStyle = style.cursors or {};
-  cursor = mkCursor {
-    inherit pkgs polarity;
-    cursor = cursorStyle.${polarity} or null;
-    accent = cursorStyle.accent or theme.accent or null;
-    flavor = theme.${polarity} or null;
-    size = cursorStyle.size or 24;
-  };
-
-  iconStyle = style.icons or {};
-  icons = mkIcon {
-    inherit pkgs polarity;
-    icon = iconStyle.${polarity} or null;
-  };
-in
-  mkConfig {
-    inherit context;
-
-    options = {
-      enable = mkEnable {
-        inherit context;
-        condition = true;
-      };
-      cursor = mkOption {
-        description = "Resolved desktop cursor contract";
-        default = cursor;
-        type = lix.styles.cursors.types.polarity.home;
-        readOnly = true;
-      };
-      icons = mkOption {
-        description = "Resolved desktop icon contract";
-        default = icons;
-        type = lix.styles.icons.types.icon.home;
-        readOnly = true;
-      };
+  #~@ Cursor configuration
+  cursor = rec {
+    name = cfg.cursors.name;
+    package = getPackage {
+      inherit pkgs;
+      target = name;
+      default = pkgs.material-cursors;
     };
+    size = cfg.cursors.size;
+  };
 
-    outputs = {
-      # DMS scans the XDG user icon directory directly. Expose the resolved
-      # package there so it is both selectable and usable by Quickshell.
-      xdg.dataFile."icons/${icons.name}".source =
-        "${icons.package}/share/icons/${icons.name}";
-
-      gtk = {
-        enable = mkForce true;
-        iconTheme = mkForce {inherit (icons) package name;};
-        cursorTheme = mkForce {inherit (cursor) package name size;};
-        gtk4.theme = null;
-      };
-
-      home = {
-        pointerCursor = mkForce {
-          gtk.enable = true;
-          x11.enable = true;
-          inherit (cursor) package name size;
-        };
-
-        sessionVariables.QS_ICON_THEME = icons.name;
-      };
-
-      qt = mkForce {
-        enable = true;
-        platformTheme.name = "gtk";
-        style.name = "kvantum";
-      };
+  #~@ Icons configuration
+  icons = rec {
+    name = cfg.icons.name;
+    package = getPackage {
+      inherit pkgs;
+      target = name;
+      default = pkgs.candy-icons;
     };
-  }
+  };
+in {
+  # _module.args = {inherit cursor icons;};
+  _module.args.${dom} = cfg;
+
+  gtk = {
+    enable = mkForce true;
+    iconTheme = mkForce {inherit (icons) package name;};
+    cursorTheme = mkForce {inherit (cursor) package name size;};
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 0;
+    gtk4 = {
+      theme = null;
+      # theme=config.gtk.theme;
+      extraConfig.gtk-application-prefer-dark-theme = 0;
+    };
+  };
+
+  home.pointerCursor = mkForce {
+    gtk.enable = true;
+    x11.enable = true;
+    inherit (cursor) package name size;
+  };
+
+  qt = mkForce {
+    enable = true;
+    platformTheme.name = "gtk";
+    style.name = "kvantum";
+  };
+}
