b5f405ae346331d8b27f0dfa42de73c40c78d34f
Craig "Craole" Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-18 15:51:40 -0500

    feat(nushell): prefer native library commands

--- Modules/nix/home/shells/core/nushell/default.nix
+++ Modules/nix/home/shells/core/nushell/default.nix
@@ -39,7 +39,18 @@ in
             $env.DOTS = ($env.DOTS? | default "${paths.repo.src.local}")
             $env.DOTS_RC = ($env.DOTS | path join ".dotsrc")
           '';
-          extraConfig = nuSection;
+          extraConfig =
+            nuSection
+            + "\n"
+            + lib.concatMapStringsSep "\n" (name: ''
+              def ${name} [...args] { ^${paths.repo.lib.nu.local}/${name}.nu ...$args }
+            '') (
+              map (name: lib.removeSuffix ".nu" name) (
+                lib.filter (name: lib.hasSuffix ".nu" name) (
+                  lib.attrNames (builtins.readDir paths.repo.lib.nu.store)
+                )
+              )
+            );
         }
         # (import ./plugins.nix {inherit pkgs;})
         (import ./settings.nix)
