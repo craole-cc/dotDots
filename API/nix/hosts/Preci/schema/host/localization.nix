b1000edb9446e3a2f2ce369ffbea67a66f082c9f
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:50:34 -0500

    refactor(schema): keep host localization generic

--- API/nix/hosts/Preci/schema/host/localization.nix
+++ API/nix/hosts/Preci/schema/host/localization.nix
@@ -1,7 +1,7 @@
 {
-  latitude = 18.015;
-  longitude = -77.49;
-  city = "Mandeville, Jamaica";
-  timeZone = "America/Jamaica";
-  defaultLocale = "en_US.UTF-8";
+  latitude = null;
+  longitude = null;
+  city = null;
+  timeZone = null;
+  defaultLocale = null;
 }
