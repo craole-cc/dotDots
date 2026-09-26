1e2d083ef0b5d99a512d37acb77290542022a396
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:56:47 -0500

    refactor(Preci): align craole password and shell fields

--- API/nix/hosts/Preci/users/craole/default.nix
+++ API/nix/hosts/Preci/users/craole/default.nix
@@ -1,7 +1,7 @@
 {
   description = "Craig 'Craole' Cole";
 
-  password = "$6$2o3rjHVZgXEuyZ97$PtmQa1PIOmKb6dAwZ0mZJUulAkJoCfj.qjJHgtusfVnIIsHmENcA7q8PV9I2PveOwdEdFWwMBgLu3a5HZavXC1";
+  hashedPassword = "$6$2o3rjHVZgXEuyZ97$PtmQa1PIOmKb6dAwZ0mZJUulAkJoCfj.qjJHgtusfVnIIsHmENcA7q8PV9I2PveOwdEdFWwMBgLu3a5HZavXC1";
 
   git = {
     name = "Craole";
