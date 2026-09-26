aa790e7d96e0eaec30321aa433f945b230ed5c79
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-26 01:53:07 -0500

    Move core boot module into directory

---
+++ API/nix/hosts/Preci/outputs/core/boot/default.nix
@@ -0,0 +1,24 @@
+{host, infrastructure, ...}: let
+  inherit (host.interface.boot.loader) device manager timeout;
+  inherit (infrastructure.core.packages) kernel;
+in {
+  boot = {
+    loader = {
+      grub = {
+        enable = manager == "grub";
+        inherit device;
+        useOSProber = true;
+        fsIdentifier = "provided";
+      };
+
+      systemd-boot = {
+        enable = manager == "systemd-boot";
+        consoleMode = "max";
+      };
+
+      inherit timeout;
+    };
+
+    kernelPackages = kernel.package;
+  };
+}
