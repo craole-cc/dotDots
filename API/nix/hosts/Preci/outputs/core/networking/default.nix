42e012e9d9638e8e195c6e70c61d0257c93f4dc9
craole-cc <134658831+craole-cc@users.noreply.github.com>
2026-09-26 10:15:28 -0500

    Preci

--- API/nix/hosts/Preci/outputs/core/networking/default.nix
+++ API/nix/hosts/Preci/outputs/core/networking/default.nix
@@ -1,10 +1,14 @@
 {
+  lix,
   host,
-  inputs,
+  registry,
   infrastructure,
   ...
 }: let
   inherit (infrastructure.core.functionalities) network;
+  inherit (lix.lists) optional;
+  inherit (lix.attrsets) recursiveUpdate;
+  inherit (registry) inputs overlays;
 in {
   networking = {
     hostName = host.name;
@@ -13,7 +17,9 @@ in {
   };
 
   nix = {
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+    };
     nixPath = [
       "nixos-config=${host.paths.roots.run}"
       "nixpkgs=${inputs.nixpkgs.path}"
@@ -21,6 +27,14 @@ in {
   };
 
   nixpkgs = {
-    pkgs = inputs.modules.nixpkgs;
+    pkgs = import inputs.nixpkgs {
+      inherit (host) system;
+      config =
+        recursiveUpdate {allowUnfree = false;}
+        host.config.nixpkgs;
+      overlays =
+        optional (host.functionalities.rust or false)
+        (import overlays.rust-overlay);
+    };
   };
 }
