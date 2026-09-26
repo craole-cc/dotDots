8740e210e59cab3839d700c04e0071ff31efc64b
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:39:19 -0500

    refactor(schema): centralize host and user defaults

---
+++ API/nix/hosts/Preci/schema/host/interface.nix
@@ -0,0 +1,28 @@
+{
+  boot = {
+    loader = {
+      manager = "systemd-boot";
+      device = "nodev";
+      timeout = 5;
+    };
+  };
+  desktops = [];
+  fonts = {
+    clock = [];
+    emoji = [];
+    material = [];
+    monospace = [];
+    sans = [];
+    serif = [];
+  };
+  themes = {
+    polarity = "dark";
+  };
+  keyboard = {
+    layout = "us";
+    variant = "";
+    swapCapsEscape = false;
+    vimKeybinds = false;
+    bindings.modifier = ["SUPER"];
+  };
+}
