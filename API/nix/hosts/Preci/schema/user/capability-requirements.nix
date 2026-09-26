03d6851698fc787b4bd92f01d9482ee0460f011f
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 23:59:48 -0500

    Refine Preci capability and package resolution

---
+++ API/nix/hosts/Preci/schema/user/capability-requirements.nix
@@ -0,0 +1,10 @@
+{
+  writing = { functionalities = []; };
+  conferencing = { functionalities = ["audio" "network" "webcam"]; };
+  development = { functionalities = []; };
+  creation = { functionalities = []; };
+  analysis = { functionalities = []; };
+  management = { functionalities = []; };
+  gaming = { functionalities = ["gpu"]; };
+  multimedia = { functionalities = ["audio" "video"]; };
+}
