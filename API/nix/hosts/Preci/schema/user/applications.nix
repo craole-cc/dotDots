7946fc502b79c65ef1ea4b94ba9c1e21db6fe145
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:59:51 -0500

    Define normalized user application contract

---
+++ API/nix/hosts/Preci/schema/user/applications.nix
@@ -0,0 +1,60 @@
+{
+  ai = {
+    primary = null;
+    secondary = null;
+    tertiary = null;
+  };
+  browser = {
+    primary = null;
+    secondary = null;
+  };
+  editor = {
+    tty = {
+      primary = null;
+      secondary = null;
+      tertiary = null;
+    };
+    gui = {
+      primary = null;
+      secondary = null;
+      tertiary = null;
+    };
+  };
+  terminal = {
+    primary = null;
+    secondary = null;
+    tertiary = null;
+  };
+  explorer = {
+    primary = null;
+    secondary = null;
+    tertiary = null;
+  };
+  launcher = {
+    primary = null;
+    secondary = null;
+    tertiary = null;
+  };
+  bar = null;
+  prompt = null;
+  allowed = [];
+  utilities = {
+    atuin.enable = false;
+    bat.enable = false;
+    btop.enable = false;
+    clock.enable = false;
+    direnv.enable = false;
+    git.enable = false;
+    gitui.enable = false;
+    github.enable = false;
+    grep.enable = false;
+    home-manager.enable = false;
+    jujutsu.enable = false;
+    nh.enable = false;
+    nix-index.enable = false;
+    topgrade.enable = false;
+    tmux.enable = false;
+    yazi.enable = false;
+    delta.enable = false;
+  };
+}
