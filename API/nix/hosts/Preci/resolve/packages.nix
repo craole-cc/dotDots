95c5e58e104625f975e4af262a4833ca4c10fe7a
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 23:40:21 -0500

    Resolve Preci package groups

--- API/nix/hosts/Preci/resolve/packages.nix
+++ API/nix/hosts/Preci/resolve/packages.nix
@@ -1,9 +1,25 @@
 {lix, ...}: let
+  expandName = groups: stack: name:
+    if lix.lists.elem name stack
+    then throw "resolve packages: cyclic package group '${name}'"
+    else if groups ? ${name}
+    then lix.lists.concatMap (expandName groups (stack ++ [name])) groups.${name}
+    else [name];
+
+  expandNames = {
+    groups,
+    names,
+  }:
+    lix.lists.concatMap (expandName groups []) names;
+
   resolveNames = {
     pkgs,
+    groups,
     names,
     context,
-  }:
+  }: let
+    expanded = lix.lists.unique (expandNames {inherit groups names;});
+  in
     lix.lists.map
     (
       name:
@@ -11,22 +27,30 @@
         then pkgs.${name}
         else throw "${context}: package '${name}' was not found in nixpkgs"
     )
-    (lix.lists.unique names);
+    expanded;
 
   resolveUser = {
     pkgs,
     user,
   }: let
+    groups = {
+      shells = user.packages.shells;
+      coding = user.packages.coding;
+      common = user.packages.common;
+      launchers = user.packages.launchers;
+    };
     names = lix.lists.unique (
       user.packages.common
       ++ user.packages.coding
       ++ user.packages.launchers
       ++ user.packages.shells
     );
+    expanded = lix.lists.unique (expandNames {inherit groups names;});
   in {
-    inherit names;
+    inherit names expanded;
     packages = resolveNames {
-      inherit pkgs names;
+      inherit pkgs groups;
+      names = expanded;
       context = "resolve principal '${user.name}'";
     };
   };
@@ -34,7 +58,21 @@ in {
   resolveHost = {
     host,
     pkgs,
-  }: {
+  }: let
+    groups = {
+      shells = host.packages.shells;
+      coding = host.packages.coding;
+      common = host.packages.common;
+      launchers = host.packages.launchers;
+    };
+    names = lix.lists.unique (
+      host.packages.common
+      ++ host.packages.coding
+      ++ host.packages.launchers
+      ++ host.packages.shells
+    );
+    expanded = lix.lists.unique (expandNames {inherit groups names;});
+  in {
     kernel = {
       name = host.packages.kernel;
       package =
@@ -44,15 +82,11 @@ in {
     };
 
     common = resolveNames {
-      inherit pkgs;
-      names =
-        host.packages.common
-        ++ host.packages.coding
-        ++ host.packages.launchers
-        ++ host.packages.shells;
+      inherit pkgs groups;
+      names = expanded;
       context = "resolve host '${host.name}'";
     };
   };
-
+  
   inherit resolveUser;
 }
