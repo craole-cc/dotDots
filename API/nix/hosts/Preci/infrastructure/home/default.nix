5c445c9f7e6ccf3e644a70fa761a5925cb163d81
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-26 01:48:47 -0500

    Build common and home infrastructure from resolved capabilities

--- API/nix/hosts/Preci/infrastructure/home/default.nix
+++ API/nix/hosts/Preci/infrastructure/home/default.nix
@@ -1,3 +1,15 @@
-{common, ...}: {
-  inherit (common) principals;
-}
+{common, lix, ...}: let
+  inherit (lix.attrsets) listToAttrs attrValues;
+  inherit (lix.lists) map unique;
+
+  home = listToAttrs (map (user: {
+    name = user.name;
+    value = {
+      packages = unique (
+        user.packages.packages
+        ++ (user.capabilities.home.packages or [])
+      );
+    };
+  }) (attrValues common.principals));
+in
+  home
