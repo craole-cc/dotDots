99766f085059d530814647206411f7229e930036
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:59:58 -0500

    Add normalized git and applications contracts to principal schema

--- API/nix/hosts/Preci/schema/user/default.nix
+++ API/nix/hosts/Preci/schema/user/default.nix
@@ -8,9 +8,11 @@
     enable = true;
     autoLogin = false;
     capabilities = {};
+    git = import ./git.nix;
     localization = import ./localization.nix;
     identities = [];
     interface = import ./interface.nix;
+    applications = import ./applications.nix;
     paths = import ./paths.nix;
     packages = import ./packages.nix;
   };
