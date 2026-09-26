5beca15b22a6ff321f5fb26cb15f6fd4ffec7c22
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-26 00:00:00 -0500

    Refine Preci capability and package resolution

--- API/nix/hosts/Preci/resolve/principal.nix
+++ API/nix/hosts/Preci/resolve/principal.nix
@@ -2,21 +2,18 @@
   resolve = {
     host,
     user,
-  }: let
-    capabilityResolution = capabilities.resolve {
-      inherit host user;
-    };
-    packageResolution = packages.resolveUser {
-      pkgs = packages.pkgs;
-      inherit user;
-    };
-  in
+  }:
     user
     // {
       resolution = {
-        inherit capabilityResolution packageResolution;
-        capabilities = capabilityResolution;
-        packages = packageResolution;
+        capabilities = capabilities.resolve {
+          inherit host user;
+        };
+
+        packages = packages.resolveUser {
+          pkgs = packages.pkgs;
+          inherit user;
+        };
       };
     };
 }
