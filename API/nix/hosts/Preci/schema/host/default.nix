e835208a5a4afcdda77aa992fd896e6ccf05ccb3
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:39:38 -0500

    refactor(schema): make host defaults authoritative

--- API/nix/hosts/Preci/schema/host/default.nix
+++ API/nix/hosts/Preci/schema/host/default.nix
@@ -1,3 +1,17 @@
 {
   functionalities = import ./functionalities.nix;
+  defaults = {
+    stateVersion = "";
+    system = "";
+    class = "nixos";
+    name = "";
+    id = null;
+    description = null;
+    localization = import ./localization.nix;
+    interface = import ./interface.nix;
+    paths = import ./paths.nix;
+    specs = {};
+    packages = import ./packages.nix;
+    principals = [];
+  };
 }
