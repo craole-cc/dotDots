4944563a90214f80f4100f1aeab406df942c20e6
Craig "Craole' Cole <134658831+craole-cc@users.noreply.github.com>
2026-09-25 21:39:40 -0500

    refactor(schema): make user defaults authoritative

--- API/nix/hosts/Preci/schema/user/default.nix
+++ API/nix/hosts/Preci/schema/user/default.nix
@@ -1,3 +1,18 @@
 {
+  defaults = {
+    name = null;
+    role = null;
+    description = null;
+    password = null;
+    hashedPassword = null;
+    enable = true;
+    autoLogin = false;
+    capabilities = {};
+    localization = import ./localization.nix;
+    identities = [];
+    interface = import ./interface.nix;
+    paths = import ./paths.nix;
+    packages = import ./packages.nix;
+  };
   capabilities = import ./capabilities.nix;
 }
